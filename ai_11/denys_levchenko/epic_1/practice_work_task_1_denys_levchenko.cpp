f444aef1295000ad277ffb3a4763fe98a10ce904
Denys Levchenko <denys.levchenko.shi.2024@lpnu.ua>
2024-09-28 21:16:59 +0300

    Epic 1 - Denys Levchenko

--- ai_11/denys_levchenko/epic_1/practice_work_task_1_denys_levchenko.cpp
+++ ai_11/denys_levchenko/epic_1/practice_work_task_1_denys_levchenko.cpp
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 #include <iostream>
 
 using namespace std;
 int main(){
 	cout << "Hello World!";
 	return 0;
+=======
+#include <iostream>
+
+using namespace std;
+int main(){
+	cout << "Hello World!";
+	return 0;
+>>>>>>> Stashed changes
 }
