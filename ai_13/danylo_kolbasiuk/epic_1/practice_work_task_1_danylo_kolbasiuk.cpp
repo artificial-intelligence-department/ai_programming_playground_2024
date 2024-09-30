93bc0e521b7599bf523b557080545b98ab18531a
Danylo Kolbasiuk <danya.kolbasiuk@gmail.com>
2024-09-30 18:12:55 +0300

    Epic 1 - Danylo Kolbasiuk

--- ai_13/danylo_kolbasiuk/epic_1/practice_work_task_1_danylo_kolbasiuk.cpp
+++ ai_13/danylo_kolbasiuk/epic_1/practice_work_task_1_danylo_kolbasiuk.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-#include <ctype.h>
 #include <stdlib.h>
 using namespace std;
+// This code calculates the profit you would get from depositing some money into the bank after a specific period of time.
+// Main sum of deposit is P, interest is r, amount of payments per year is n(only works if n=1, 4 or 12), depositing time in years is t.
+// The code uses this formula to calculate the profit: A=P*(1+(r/(100*n))^(n*t))
+// The 100 is included because the input for interest is an integer, but we have to convert it to a decimal instead of a percentage (1%=0.01)
+// The code also checks whether all the input values are positive integers. Otherwise, the code will output a warning and will ask to type in another value.
+// I also tried to modify the code to check for incorrect data types, but I couldn't get it to work :/
 static int P, t, n, r;
 void formula()
 {
@@ -12,18 +17,22 @@ void formula()
     double temp3=pow(temp1, temp2);
     double A=P*temp3;
     double S=A-double(P);
-    printf("Main sum of investment: %u", P);
+    printf("Main sum of deposit: %u", P);
     printf("\n");
     printf("Interest: %u", r);
     printf("%%");
     printf("\n");
-    printf("Future sum of investment: %0.2f", A);
+    printf("Depositing time: %u", t);
+    printf("\n");
+    printf("Amount of payments per year: %u", n);
+    printf("\n");
+    printf("Future sum of deposit: %0.2f", A);
     printf("\n");
     printf("Profit: %0.2f", S);
 }
 void input()
 {
-    printf("Enter main sum of investment: ");
+    printf("Enter main sum of deposit: ");
     do {
         scanf("%u", &P);
         if(P<1)  {
@@ -58,4 +67,5 @@ int main()
 {
     input();
     formula();
+    return 0;
 }
