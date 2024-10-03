cae1d102ee4404ebb7ff8f06fdeca1817b601f42
Nazik <nazikkryvycko@gmail.com>
2024-10-03 19:26:08 +0300

    Nazar Kryvycko - UPDATE

--- ai_12/nazar_kryvychko/epic_1/self_practice_work_algotester_task_1_nazar_kryvychko.cpp
+++ ai_12/nazar_kryvychko/epic_1/self_practice_work_algotester_task_1_nazar_kryvychko.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 
 int main() {
-    // Вхідні дані
     int l, w, u, d;
     cin >> l >> w >> u >> d;
 
-    // Перевіряємо, чи може пінгвін випити коктейль
     if (w >= l && (u + d) >= l) {
         cout << "Three times Sex on the Beach, please!" << endl;
     } else {
         cout << "Forget about the cocktails, man!" << endl;
     }
-
     return 0;
 }
