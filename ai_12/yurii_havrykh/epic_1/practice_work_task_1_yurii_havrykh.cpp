dab31f73416717c606daad2abac1ec4b25a316b7
Yurii Havrykh <uragavrih031@gmail.com>
2024-10-08 21:35:50 +0300

    Epic_1_Yurii_Havrykh

--- ai_12/yurii_havrykh/epic_1/practice_work_task_1_yurii_havrykh.cpp
+++ ai_12/yurii_havrykh/epic_1/practice_work_task_1_yurii_havrykh.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n,t,P;
-    float A,r;
+    float A,r,n,t,P;
 
     printf("Enter the principal amount of the investment:");
-    scanf("%d",&P);
+    scanf("%f",&P);
     printf("Enter the annual interest rate:");
     scanf("%f",&r);
     printf("Enter the number of interest charges per year:");
-    scanf("%d",&n);
+    scanf("%f",&n);
     printf("Enter the time for which the money is invested, in years:");
-    scanf("%d",&t);
+    scanf("%f",&t);
 
     A=P * pow(( 1 + ( r / n ) ) ,n * t );
 
     printf("invested funds: ");
-    printf("%d\n",P);
+    printf("%.4f\n",P);
     printf("the total investment amount: ");
-    printf("%f\n",A);
+    printf("%.4f\n",A);
     printf("the amount of earnings: ");
-    printf("%f\n",A-P);
+    printf("%.4f\n",A-P);
 }
