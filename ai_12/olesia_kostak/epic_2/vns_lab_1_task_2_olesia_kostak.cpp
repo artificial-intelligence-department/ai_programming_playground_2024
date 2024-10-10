f3baefa417334bcc3ff54a1347d081c52743d4d1
Olesia Kostak <olesiakostak@.com>
2024-10-10 22:13:00 +0300

    vns lab 1 task 2 improvement

--- ai_12/olesia_kostak/epic_2/vns_lab_1_task_2_olesia_kostak.cpp
+++ ai_12/olesia_kostak/epic_2/vns_lab_1_task_2_olesia_kostak.cpp
@@ -6,10 +6,16 @@ int main()
     std::cout << "Enter two similar integers:";
     std::cin >> n >> m;
 
+    //Calculating
+    int res1, res2, res3;
+    res1 = n++ - m;
+    res2 = m-- > n;
+    res3 = n-- > m;
+
     //Printing our results
-    std::cout << (n ++ -m) << std::endl;
-    std::cout << (m-- > n) << std::endl;
-    std::cout << (n-- > m) << std::endl;
+    std::cout << "1) n++ - m: " << res1 << std::endl;
+    std::cout << "2) m-- > n: " << res2 << std::endl;
+    std::cout << "3) n-- > m: " << res3 << std::endl; 
 
     return 0;
 }
