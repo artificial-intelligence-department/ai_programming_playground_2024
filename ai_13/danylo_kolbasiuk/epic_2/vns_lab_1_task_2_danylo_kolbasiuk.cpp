c55bc0bc71aef02e998312388374543e28f06737
Danylo Kolbasiuk <danya.kolbasiuk@gmail.com>
2024-10-12 00:01:41 +0300

    Epic 2 - Danylo Kolbasiuk

---
+++ ai_13/danylo_kolbasiuk/epic_2/vns_lab_1_task_2_danylo_kolbasiuk.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n,m;
+    cin >> n >> m;
+    long long a=n---m;
+    long long b=m--<n;
+    long long c=n++>m;
+    cout << a << endl << b << endl << c;
+}
