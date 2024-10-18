7552f8a0003611059ad4fc333c6741ec59452beb
Mykola Fedoryshyn <koliafedoryshyn@gmail.com>
2024-10-18 10:32:47 +0300

    Epic 1 - Mykola Fedoryshyn

--- ai_11/mykola_fedoryshyn/epic_1/practice_work_task_1_mykola_fedoryshyn.cpp
+++ ai_11/mykola_fedoryshyn/epic_1/practice_work_task_1_mykola_fedoryshyn.cpp
@@ -1,10 +1,28 @@
-#include "iostream";
-#include "string";
+#include <iostream>
+#include <string.h>
+#include <cmath>
 
+main(){
+    int P, n;
+    double r, t, A, power;
 
-main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a+b;
-    return 0; 
+    printf("Enter deposit sum: ");
+    scanf("%d", &P);
+
+    printf("Annual interest rate: ");
+    scanf("%lf", &r);
+
+    printf("Number of profits: ");
+    scanf("%d", &n);
+
+    printf("Time(in years): ");
+    scanf("%lf", &t);
+
+    A = P * pow((1 + (r / n)), (n * t));;
+
+    printf("Invested funds: %d \n", P);
+    printf("General sum of funds: %.3lf \n", A);
+    printf("Sum of income: %.3lf \n", (A-P));
+    return 0;
+
 }
