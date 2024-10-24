711fa04e42b7bde4dda34a0377cd89fbb94fa796
Olesia Kostak <olesiakostak@.com>
2024-10-25 01:06:05 +0300

    Epic 3

--- ai_12/olesia_kostak/epic_3/vns_lab_7_task_2_variant_1_olesia_kostak.cpp
+++ ai_12/olesia_kostak/epic_3/vns_lab_7_task_2_variant_1_olesia_kostak.cpp
@@ -14,10 +14,13 @@ std::complex<double> Multiply(std::complex<double> a, std::complex<double> b)
 
 int main()
 {
-    std::complex<double> re1, im1, re2, im2;
+    std::complex<double> num1, num2;
+    double re1, im1, re2, im2;
     std::cout << "Enter two complex numbers:" << std::endl;
     std::cin >> re1 >> im1 >> re2 >> im2;
-    std::cout << "Multiple: " << Multiply((re1, im1), (re2, im2)) <<std::endl;
+    num1 = std::complex<double>(re1, im1);
+    num2 = std::complex<double>(re2, im2);
+    std::cout << "Multiple: " << Multiply(num1, num2) <<std::endl;
 
     long double a, b;
     std::cout << "Enter two real numbers:" << std::endl;
