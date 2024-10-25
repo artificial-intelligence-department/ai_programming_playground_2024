ff3d458c409f1142866ec3f03e486c29d7c2ee67
Stefan <stefan.shyjka@gmail.com>
2024-10-25 14:25:24 +0300

    added lab 7 task 2

--- ai_12/stefan_shyika/Epic3/vns_lab_7_task_2_variant_1_stefan_shyika.cpp
+++ ai_12/stefan_shyika/Epic3/vns_lab_7_task_2_variant_1_stefan_shyika.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <cstdarg>
-#include <algorithm>
+#include <string>
+#include <cstring>
+#include <sstream>
 
 using namespace std;
 
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+int func(int arr[], int len){
+    int max = arr[0];
+
+    for(int i = 1; i < len; i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
     }
-    return a;
+    return max;
 }
 
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
-}
-
-int lcmOfMany(int count, ...) {
-    va_list args;
-    va_start(args, count);
+int func(string str){
+    stringstream strStream(str);
+    string word;
+    int max = 0;
     
-    int result = va_arg(args, int);
-    
-    for (int i = 1; i < count; i++) {
-        int next = va_arg(args, int);
-        result = lcm(result, next);
+    while (strStream >> word) {
+        if (word.length() > max) {
+            max = word.length();
+        }
     }
     
-    va_end(args);
-    return result;
+    return max;
 }
 
-int main() {
-    cout << "НСК(3, 5, 6): " << lcmOfMany(3, 3, 5, 6) << endl;
-    cout << "НСК(2, 3, 5, 6, 10): " << lcmOfMany(5, 2, 3, 5, 6, 10) << endl;
-    cout << "НСК(1, 2, 3, 4, 5, 6): " << lcmOfMany(6, 1, 2, 3, 4, 5, 6) << endl;
+int main(){
+    int arr[] = {1, 5 , 10 , 4, 15};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    string str = "Hello, I am Stefan";
+
+    cout << "Максимальний елемент у масиві: " << func(arr, len) << endl;
+
+    cout << "Довжина найдовшого слова: " << func(str) << endl;
 }
