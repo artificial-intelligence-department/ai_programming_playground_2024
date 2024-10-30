60276e185bc70dadf0964465a8a4ed1151f892f8
mikamika06 <mikyseasbk@gmail.com>
2024-10-30 20:19:49 +0200

    mainError1

--- ai_11/savkov_oleksandr/epic_2/Code/vns_lab_1_task_2_variant_23_oleksandr_savkov.cpp
+++ ai_11/savkov_oleksandr/epic_2/Code/vns_lab_1_task_2_variant_23_oleksandr_savkov.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+
+int main() {
+    int n;
+    int m;
+
+    cout << "Enter value of n: " << endl;
+    cin >> n;
+    cout << "Enter value of m: " << endl;
+    cin >> m;
+
+    // 1) n---m
+    int result1 = n---m;
+    cout << "n---m:   " << result1 << ", n = " << n << ", m = " << m << endl;
+
+    // 2) m--<n
+    bool result2 = m--<n;
+    cout << "m--<n: " << result2 << ", n = " << n << ", m = " << m << endl;
+
+    // 3) n++>m
+    bool result3 = n++>m;
+    cout << "n++>m:   " << result3 << ", n = " << n << ", m = " << m << endl;
+
+    return 0;
+}
