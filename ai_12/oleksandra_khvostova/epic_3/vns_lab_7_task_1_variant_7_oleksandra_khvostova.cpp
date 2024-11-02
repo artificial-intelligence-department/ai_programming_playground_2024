024903ce343089628aee3d9adccefc6ecd20c30a
oleksandra_khvostova <hvosto28@gmail.com>
2024-11-02 21:24:21 +0200

    Epic 3 - Oleksandra Khvostova

--- ai_12/oleksandra_khvostova/epic_3/vns_lab_7_task_1_variant_7_oleksandra_khvostova.cpp
+++ ai_12/oleksandra_khvostova/epic_3/vns_lab_7_task_1_variant_7_oleksandra_khvostova.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-template <typename type>
-type min(int count, ...) {
+template <typename T>
+T min(int count, ...) {
     va_list args;
     va_start(args, count);
 
-    type min_val = numeric_limits<type>::max();
+    T min_val = numeric_limits<T>::max();
 
     for (int i = 0; i < count; ++i) {
-        type val = va_arg(args, type);
+        T val = va_arg(args, T);
         if (val < min_val) {
             min_val = val;
         }
@@ -23,9 +23,9 @@ type min(int count, ...) {
 }
 
 int main() {
-    cout << "Minimum of 5 ints: " << min<int>(5, 10, 20, 3, 40) << endl;
-    cout << "Minimum of 10 doubles: " << min<double>(10, 12.3, 44.5, 22.1, 37.7, 30.5, 567.9, 67.4, 4.5, 89.2) << endl;
-    cout << "Minimum of 12 doubles: " << min<double>(12, 12.3, 44.5, 78.9, 1.1, 22.6, 37.8, 30.5, 567.9, 67.4, 4.5, 123.1) << endl;
+    cout << "Minimum of 5 ints: " << min<int>(5, 10, 20, 3, 40, 50) << endl;
+    cout << "Minimum of 10 doubles: " << min<double>(10, 12.3, 44.5, 22.1, 37.7, 30.5, 567.9, 67.4, 4.5, 7.7,89.2) << endl;
+    cout << "Minimum of 12 doubles: " << min<double>(12, 12.3, 44.5, 78.9, 1.1, 22.6, 37.8, 30.5, 567.9, 67.4, 4.5, 123.1, 89.2) << endl;
 
     return 0;
 }
