4e2288c4234d54265f8e3286894e73f0862b23a4
Anton Mykhalchuk <anton.mykhalchuk.shi.2024@lpnu.ua>
2024-11-03 23:09:06 +0200

    Epic 2 Anton Mykhalchuk

---
+++ ai_12/anton_mykhalchuk/epic_2/practice_work_self_algotester_tasks_anton_mykhalchuk.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    double rn; 
+    int r, n;
+
+    cin >> n >> r;
+
+    rn = r/sqrt(n);
+
+    cout << rn;
+
+    return 0;
+}
