06faf9341ade97477d133b5abe23ffa625032952
taras-ivanyk <taras.ivanyk.shi.2024@lpnu.ua>
2024-11-04 18:59:49 +0200

    Add vns lab 7 task 1 and 2

---
+++ ai_12/taras_ivanyk/epic_3/vns_lab_7_task_2_variant_18_taras_ivanyk.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+// Функція для визначення року за старояпонським календарем
+std::string getJapaneseYear(int year) {
+    const std::string years[] = {
+        "Щур", "Бик", "Тигр", "Кролик", "Дракон", "Змія",
+        "Кінь", "Коза", "Мавпа", "Півень", "Собака", "Кабан"
+    };
+    int index = (year - 4) % 12;
+    return years[index];
+}
+
+// Функція для визначення знака зодіаку за місяцем
+std::string getMonth(const std::string& month) {
+    if (month == "January" || month == "February") return "Водолій";
+    if (month == "February" || month == "March") return "Риби";
+    if (month == "March" || month == "April") return "Овен";
+    if (month == "April" || month == "May") return "Телець";
+    if (month == "May" || month == "June") return "Близнюки";
+    if (month == "June" || month == "July") return "Рак";
+    if (month == "July" || month == "August") return "Лев";
+    if (month == "August" || month == "September") return "Діва";
+    if (month == "September" || month == "October") return "Терези";
+    if (month == "October" || month == "November") return "Скорпіон";
+    if (month == "November" || month == "December") return "Стрілець";
+    if (month == "December" || month == "January") return "Козеріг";
+    
+    return "Unknown month";
+}
+
+int main() {
+ 
+    std::cout << "Enter a year: ";
+    int year;
+    std::cin >> year;
+    std::cout << "Ваш рік за старояпонським календарем, це: " << getJapaneseYear(year) << std::endl;
+
+    std::cout << "Enter a month ";
+    std::string month;
+    std::cin >> month;
+    std::cout << "Ваш знак зодіаку: " << getMonth(month) << std::endl;
+
+    return 0;
+}
