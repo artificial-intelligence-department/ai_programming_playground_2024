7ce17b3e8d9bc7a8e81a32fa0f98e19cc05c6147
taras-ivanyk <taras.ivanyk.shi.2024@lpnu.ua>
2024-11-05 13:28:44 +0200

    Changes in lab 7 task 2

--- ai_12/taras_ivanyk/epic_3/vns_lab_7_task_2_variant_18_taras_ivanyk.cpp
+++ ai_12/taras_ivanyk/epic_3/vns_lab_7_task_2_variant_18_taras_ivanyk.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
 
-// Функція для визначення року за старояпонським календарем
-std::string getJapaneseYear(int year) {
+std::string get(int year) {
     const std::string years[] = {
         "Щур", "Бик", "Тигр", "Кролик", "Дракон", "Змія",
         "Кінь", "Коза", "Мавпа", "Півень", "Собака", "Кабан"
@@ -10,9 +9,7 @@ std::string getJapaneseYear(int year) {
     int index = (year - 4) % 12;
     return years[index];
 }
-
-// Функція для визначення знака зодіаку за місяцем
-std::string getMonth(const std::string& month) {
+std::string get(const std::string& month) {
     if (month == "January" || month == "February") return "Водолій";
     if (month == "February" || month == "March") return "Риби";
     if (month == "March" || month == "April") return "Овен";
@@ -34,12 +31,12 @@ int main() {
     std::cout << "Enter a year: ";
     int year;
     std::cin >> year;
-    std::cout << "Ваш рік за старояпонським календарем, це: " << getJapaneseYear(year) << std::endl;
+    std::cout << "Ваш рік за старояпонським календарем, це: " << get(year) << std::endl;
 
     std::cout << "Enter a month ";
     std::string month;
     std::cin >> month;
-    std::cout << "Ваш знак зодіаку: " << getMonth(month) << std::endl;
+    std::cout << "Ваш знак зодіаку: " << get(month) << std::endl;
 
     return 0;
 }
