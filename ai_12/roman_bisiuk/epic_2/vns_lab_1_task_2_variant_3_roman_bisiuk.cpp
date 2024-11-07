4078acf28539a91496d56b8688327061165ec01c
Roman Bisiuk <roman.bisiuk.shi.2024@lpnu.ua>
2024-11-07 22:55:43 +0200

    epic_2

---
+++ ai_12/roman_bisiuk/epic_2/vns_lab_1_task_2_variant_3_roman_bisiuk.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cout << "n: ";
+    cin >> n;
+    cout << "m: ";
+    cin >> m;
+
+    cout << n+++m << endl;
+    cout << (m-- < n) << endl;
+    cout << (n++ > m) << endl;
+
+}
