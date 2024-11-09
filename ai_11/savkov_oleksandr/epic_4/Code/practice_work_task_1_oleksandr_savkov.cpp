bc72aaf30b341a69cad0baf86fa478f32633b072
mikamika06 <mikyseasbk@gmail.com>
2024-11-09 19:02:48 +0200

    selfRewied 2

--- ai_11/savkov_oleksandr/epic_4/Code/practice_work_task_1_oleksandr_savkov.cpp
+++ ai_11/savkov_oleksandr/epic_4/Code/practice_work_task_1_oleksandr_savkov.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-bool IsPalindrome(const string& line, int start, int end) {
-    bool tmp = true;  
-    if (start < end) {
-        if (line[start] == line[end]) {
-            tmp = IsPalindrome(line, start + 1, end - 1);
-        } else {
-            tmp = false;
-        }
+bool numCount(const string& word, int start, int end) {
+    if (start >= end) {
+        return true;
     }
-    return tmp;
-}
-
-bool IsPalindrome(int test_int) {
-    string test = to_string(test_int);
-    return IsPalindrome(test, 0, test.length() - 1);
-}
-
-void Print(bool res, const string& type) {
-    if (res) {
-        cout << type << " є паліндромом!" << endl;
-    } else {
-        cout << type << " не є паліндромом..." << endl;
+    if (word[start] != word[end]) {
+        return false;
     }
+    return numCount(word, start + 1, end - 1);
 }
 
-int main() {
-    int test_int;
-    cout << "Введіть число для перевірки: ";
-    cin >> test_int;
-    bool res_int = IsPalindrome(test_int);
-    Print(res_int, "Число");
+bool isPolindrom(const string& word) {
+    return numCount(word, 0, word.size() - 1);
+}
 
-    string test_str;
+bool isPolindrom(int number) {
+    string numberStr = to_string(number);
+    return numCount(numberStr, 0, numberStr.size() - 1);
+}
+
+int main(){
+    string word;
     cout << "Введіть слово для перевірки: ";
-    cin >> test_str;
-    bool res_str = IsPalindrome(test_str, 0, test_str.length() - 1);
-    Print(res_str, "Слово");
+    cin >> word;
+    if (isPolindrom(word))
+    {
+        cout << "Слово є паліндромом." << endl; 
+    }else{
+        cout << "Слово не є паліндромом." << endl;
+    }
+    
+    int number;
+    cout << "Введіть число: ";
+    cin >> number;
 
+    if (isPolindrom(number))
+    {
+        cout << "Число є паліндромом." << endl;
+    }else{
+        cout << "Число не є паліндромом." << endl;
+    }
     return 0;
 }
