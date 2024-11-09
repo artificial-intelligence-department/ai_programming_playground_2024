be5a88f2d07983b43db6706519c52cb99b35972f
Artem <akravchenko807@gmail.com>
2024-11-09 18:47:20 +0200

    Epic 3 - Artem Kravchenko

--- ai_11/artem_kravchenko/epic_3/vns_lab_7_task_2_variant_24_artem_kravchenko.cpp
+++ ai_11/artem_kravchenko/epic_3/vns_lab_7_task_2_variant_24_artem_kravchenko.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
-void rem(vector<int>& vec){
-    vec.erase(remove_if(vec.begin(), vec.end(), [](int x) { return (x % 2) == 0 ; }), vec.end());
+void remove(vector<int>& vec) {
+    vector<int> result;
+    for (int x : vec) {
+        if (x % 2 != 0) {
+            result.push_back(x);
+        }
+    }
+    vec = result;
 }
 
-void rem(vector<string>& vec){
-    vec.erase(remove_if(vec.begin(), vec.end(), [&](const string& word) {
-        auto index = &word - &vec[0];
-        return index % 2 != 0;
-    }), vec.end());
+void remove(vector<string>& vec) {
+    vector<string> result;
+    for (int i = 0; i < vec.size(); ++i) {
+        if (i % 2 == 0) {
+            result.push_back(vec[i]);
+        }
+    }
+    vec = result;
 }
 
-int main(){
-    int size_n = 0;
-    cout << "введіть кількість елементів: "<< endl;
+int main() {
+    int size_n;
+    cout << "Введіть кількість елементів: " << endl;
     cin >> size_n;
 
     vector<int> numbers(size_n);
     cout << "Введіть " << size_n << " чисел: " << endl;
-    for (int i = 0; i < size_n; i++)
-    {
+    for (int i = 0; i < size_n; i++) {
         cin >> numbers[i];
     }
-    rem(numbers);
+    
+    remove(numbers);
     cout << "Результат видалення парних чисел: " << endl;
-    for (int i = 0; i < numbers.size(); i++)
-    {
-        cout << numbers[i] << " " << endl;
+    for (int num : numbers) {
+        cout << num << " ";
     }
+    cout << endl;
 
-    int size_w = 0;
+    int size_w;
     cout << "Введіть кількість слів: " << endl;
     cin >> size_w;
     cin.ignore();
     
     vector<string> words(size_w);
-    cout << "Введіть " << size_w << " слів:" << endl;
-    for (int i = 0; i < size_w; i++)
-    {
+    cout << "Введіть " << size_w << " слів: " << endl;
+    for (int i = 0; i < size_w; i++) {
         getline(cin, words[i]);
     }
 
-     rem(words);
-     cout << "Результат видалення парних слів: " << endl;
-    for (const string& word : words)
-    {
-        cout << word << " " ;
+    remove(words);
+    cout << "Результат видалення слів з непарними індексами: " << endl;
+    for (const string& word : words) {
+        cout << word << " ";
     }
-    
-    
-    return 0;
+    cout << endl;
 
+    return 0;
 }
