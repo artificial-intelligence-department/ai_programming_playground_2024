735054c9bce59661d41a543fdc7fe47c2aaa96fb
mikamika06 <mikyseasbk@gmail.com>
2024-11-09 18:55:03 +0200

    selRewied

--- ai_11/savkov_oleksandr/epic_4/Code/practice_work_task_1_oleksandr_savkov.cpp
+++ ai_11/savkov_oleksandr/epic_4/Code/practice_work_task_1_oleksandr_savkov.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
-
 using namespace std;
 
-bool isPalindrome(const string& text) {
-    string reversedText = text;
-    reverse(reversedText.begin(), reversedText.end());
-    return text == reversedText;
+bool IsPalindrome(const string& line, int start, int end) {
+    bool tmp = true;  
+    if (start < end) {
+        if (line[start] == line[end]) {
+            tmp = IsPalindrome(line, start + 1, end - 1);
+        } else {
+            tmp = false;
+        }
+    }
+    return tmp;
 }
 
-bool isPalindrome(int value) {
-    string valueStr = to_string(value);
-    string reversedValueStr = valueStr;
-    reverse(reversedValueStr.begin(), reversedValueStr.end());
-    return valueStr == reversedValueStr;
+bool IsPalindrome(int test_int) {
+    string test = to_string(test_int);
+    return IsPalindrome(test, 0, test.length() - 1);
+}
+
+void Print(bool res, const string& type) {
+    if (res) {
+        cout << type << " є паліндромом!" << endl;
+    } else {
+        cout << type << " не є паліндромом..." << endl;
+    }
 }
 
 int main() {
-    string inputText;
-    cout << "Введіть слово для перевірки: ";
-    cin >> inputText;
-    if (isPalindrome(inputText)) {
-        cout << "Слово є паліндромом." << endl;
-    } else {
-        cout << "Слово не є паліндромом." << endl;
-    }
+    int test_int;
+    cout << "Введіть число для перевірки: ";
+    cin >> test_int;
+    bool res_int = IsPalindrome(test_int);
+    Print(res_int, "Число");
 
-    int inputNumber;
-    cout << "Введіть число: ";
-    cin >> inputNumber;
-    if (isPalindrome(inputNumber)) {
-        cout << "Число є паліндромом." << endl;
-    } else {
-        cout << "Число не є паліндромом." << endl;
-    }
+    string test_str;
+    cout << "Введіть слово для перевірки: ";
+    cin >> test_str;
+    bool res_str = IsPalindrome(test_str, 0, test_str.length() - 1);
+    Print(res_str, "Слово");
 
     return 0;
 }
