d8ec6926a9f0753721414cac60ba7cf051dca619
Yuliia Lazarevych <yulyalazar0603@gmail.com>
2024-11-12 18:49:36 +0200

    Epic 3 -Yuliia Lazarevych

---
+++ ai_12/yuliia_lazarevych/epic_3/codes/vns_lab_7_task_1_variant_10_yuliia_lazarevych.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <cstdarg>
+
+using namespace std;
+
+// Функція kvadr для перевірки точних квадратів серед змінної кількості параметрів
+int kvadr(int numArgs, ...) {
+    va_list args;
+    va_start(args, numArgs);
+
+    int count = 0;
+    for (int i = 0; i < numArgs; ++i) {
+        int num = va_arg(args, int);
+        int sqrtNum = static_cast<int>(sqrt(num));
+        if (sqrtNum * sqrtNum == num) {
+            ++count;
+        }
+    }
+
+    va_end(args);
+    return count;
+}
+
+int main() {
+    // Виклик функції kvadr з трьома параметрами
+    cout << "Кількість точних квадратів (3 параметри): " << kvadr(3, 2, 4, 9) << endl;
+    
+    // Виклик функції kvadr з сімома параметрами
+    cout << "Кількість точних квадратів (7 параметрів): " << kvadr(7, 1, 4, 8, 9, 16, 23, 25) << endl;
+    
+    // Виклик функції kvadr з одинадцятьма параметрами
+    cout << "Кількість точних квадратів (11 параметрів): " << kvadr(11, 3, 6, 9, 12, 16, 25, 36, 48, 49, 64, 100) << endl;
+
+    return 0;
+}
