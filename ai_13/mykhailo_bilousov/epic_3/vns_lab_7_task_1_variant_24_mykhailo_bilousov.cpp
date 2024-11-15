d0b92706c03f083f41729eb79cf3e3c8e1109e68
M <mykhailo.bilousov.shi.2024@lpnu.ua>
2024-11-15 05:26:30 +0200

    add drawios

--- ai_13/mykhailo_bilousov/epic_3/vns_lab_7_task_1_variant_24_mykhailo_bilousov.cpp
+++ ai_13/mykhailo_bilousov/epic_3/vns_lab_7_task_1_variant_24_mykhailo_bilousov.cpp
@@ -26,35 +26,18 @@ T squaren(vector<array<T,2>> points) { // shoelace algorithm
     }
     area += points[last][0]*points[0][1];
     area -= points[last][1]*points[0][0];
-    area = abs(area/2);
-    return area;
+    return fabs(area/2);
 }
 
 int main() {
     vector<array<double,2>> triangle = { // area 4.5
         {0,0}, {0,3}, {3,3}
     };
-    vector<array<double,2>> sq = { // area 9
-        {0,0}, {0,3}, {3,3}, {3,0}
-    };
-    vector<array<double,2>> rect = { // area 15
-        {0,0}, {0,5}, {3,5}, {3,0}
-    };
-    vector<array<double,2>> sq2 = { // area 10
-        {0,0}, {-1,3}, {2,4}, {3,1}
-    };
-    vector<array<double,2>> poly1 = {
-        {1,1},{0,2},{1,4},{2,5},{6,5},{3.15,3},{1,1.35}
-    };
-    vector<array<double,2>> poly2 = {
+    vector<array<double,2>> poly = {
         {1.03,1},{0,2.007},{1.7223,4},{2.11,5},{6.2,5.02},{3.15,3},{1,1.35}
     };
 
     cout << square(triangle[0],triangle[1],triangle[2]) << endl;
     cout << squaren(triangle) << endl;
-    cout << squaren(sq) << endl;
-    cout << squaren(rect) << endl;
-    cout << squaren(sq2) << endl;
-    cout << squaren(poly1) << endl;
-    cout << squaren(poly2) << endl;
+    cout << squaren(poly) << endl;
 }
