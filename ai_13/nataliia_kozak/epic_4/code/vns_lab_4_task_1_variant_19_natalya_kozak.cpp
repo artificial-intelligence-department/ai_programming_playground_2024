ad97e532d065668a2eab8b53d03cb6fef60272b7
Natalya <nataliia.kozak.shi.2024@lpnu.ua>
2024-11-15 19:01:16 +0200

    modified one program

--- ai_13/nataliia_kozak/epic_4/code/vns_lab_4_task_1_variant_19_natalya_kozak.cpp
+++ ai_13/nataliia_kozak/epic_4/code/vns_lab_4_task_1_variant_19_natalya_kozak.cpp
@@ -34,12 +34,6 @@ void removeOdd(int arr[], int &n) {
     n = newSize;
 }
 
-void print(const int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
 
 int main() {
     int arr[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
