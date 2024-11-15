15d911bad19a3e3d6465dd1506e15fa00c35870f
Sofia Kuts <sofiakuts@Sofia.local>
2024-11-15 21:39:18 +0200

    Epic 1 - Sofia Kuts

---
+++ ai_12/sofia_kuts/epic_1/self_practice_work_algotester_task_1_sofia_kuts.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int A;
+    int B;
+    cout <<" Введіть число A:";
+    cin >> A;
+    cout <<" Введіть число B:";
+    cin >> B;
+    cout << " A + B:" << A + B << endl;
+
+    return 0;
+}
