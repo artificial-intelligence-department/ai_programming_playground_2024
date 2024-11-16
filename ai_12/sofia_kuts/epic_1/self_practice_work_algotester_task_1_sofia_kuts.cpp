e3c36803c3c92447a52284b8d1f6cad428fac155
Sofia Kuts <sofiakuts@Sofia.local>
2024-11-16 18:10:01 +0200

    Epic 1 - Sofia Kuts

--- ai_12/sofia_kuts/epic_1/self_practice_work_algotester_task_1_sofia_kuts.cpp
+++ ai_12/sofia_kuts/epic_1/self_practice_work_algotester_task_1_sofia_kuts.cpp
@@ -4,11 +4,8 @@ using namespace std;
 int main () {
     int A;
     int B;
-    cout <<" Введіть число A:";
-    cin >> A;
-    cout <<" Введіть число B:";
-    cin >> B;
-    cout << " A + B:" << A + B << endl;
+    cin >> A >> B;
+    cout << A + B << endl;
 
     return 0;
 }
