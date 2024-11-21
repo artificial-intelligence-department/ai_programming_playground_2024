dcecec9a1bc158e273214a809a4f63911130dca0
Khodatskaa <khodatskaalina@gmail.com>
2024-11-21 19:26:26 +0200

    Epic 4 - Alina Khodatska

---
+++ ai_13/alina_khodatska/epic_4/code/self_practice_work_alina_khodatska.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    // Зчитуємо кількість рядків
+    int N;
+    cin >> N;
+    cin.ignore();  // ігноруємо переведення на новий рядок після N
+
+    int totalRituals = 0;
+
+    // Обробляємо кожен рядок
+    for (int i = 0; i < N; ++i) {
+        string line;
+        getline(cin, line);
+
+        stringstream ss(line);
+        string word;
+
+        // Обробляємо кожне слово в рядку
+        while (ss >> word) {
+            int rituals = 0;
+            bool inRitual = false;
+
+            // Перевіряємо кожен символ в слові
+            for (char c : word) {
+                if (c == '#') {
+                    if (!inRitual) {
+                        // Початок нової послідовності пошкоджених символів
+                        rituals++;
+                        inRitual = true;
+                    }
+                }
+                else {
+                    // Завершення послідовності пошкоджених символів
+                    inRitual = false;
+                }
+            }
+
+            totalRituals += rituals;
+        }
+    }
+
+    // Виводимо загальну кількість ритуалів
+    cout << totalRituals << endl;
+
+    return 0;
+}
