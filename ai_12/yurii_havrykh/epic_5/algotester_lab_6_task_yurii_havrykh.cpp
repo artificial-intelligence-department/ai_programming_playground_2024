5f8a5c78d154ceb1176c267fcb51ce401591e2ba
Yurii Havrykh <uragavrih031@gmail.com>
2024-11-22 09:11:13 +0200

    fix algotester_lab_6

--- ai_12/yurii_havrykh/epic_5/algotester_lab_6_task_yurii_havrykh.cpp
+++ ai_12/yurii_havrykh/epic_5/algotester_lab_6_task_yurii_havrykh.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<string> board(8);
 
 bool could_king(int x, int y) {
-    if (((0 <= x) && (x <= 7) && (0 <= y) && (y <= 7)) && (board[x][y] == 'K'))
+    if (((0 <= x) && (x <8) && (0 <= y) && (y <8)) && (board[x][y] == 'K'))
         return true;
     else
         return false;
@@ -26,22 +26,22 @@ bool king(int x, int y) {
     return flag;
 }
 
-bool tyra(int x, int y) {
+bool tyra(int x, int y,char type) {
     bool flag = false;
-    for (int i = 0; i < 7; i++)
-        if ((board[x][i] == 'R') || (board[i][y]) == 'R')
+    for (int i = 0; i < 8; i++)
+        if ((board[x][i] == type) || (board[i][y]) == type)
             flag = true;
     return flag;
 }
 
-bool could_slon(int x, int y) {
-    if (((0 <= x) && (x <= 7) && (0 <= y) && (y <= 7)) && (board[x][y] == 'B'))
+bool could_slon(int x, int y,char type) {
+    if (((0 <= x) && (x <8) && (0 <= y) && (y <8)) && (board[x][y] == type))
         return true;
     else
         return false;
 }
 
-bool slon(int x, int y) {
+bool slon(int x, int y, char type) {
     bool flag = false;
     vector<pair<int, int>> offset = {{1,  1},
                                      {1,  -1},
@@ -49,7 +49,7 @@ bool slon(int x, int y) {
                                      {-1, -1}};
     for (int i = -7; i < 8; i++) {
         for (int j = 0; j < 4; j++) {
-            if (could_slon(x + i * offset[j].first, y + i * offset[j].second))
+            if (could_slon(x + i * offset[j].first, y + i * offset[j].second,type))
                 flag = true;
         }
     }
@@ -57,7 +57,7 @@ bool slon(int x, int y) {
 }
 
 bool could_horse(int x, int y) {
-    if (((0 <= x) && (x <= 7) && (0 <= y) && (y <= 7)) && (board[x][y] == 'N'))
+    if (((0 <= x) && (x <8) && (0 <= y) && (y <8)) && (board[x][y] == 'N'))
         return true;
     else
         return false;
@@ -90,13 +90,13 @@ bool pishak(int x, int y) {
 }
 
 string find_result(int x, int y) {
-    string result = "";
-    if (slon(x, y)) result += "B";
+    string result ;
+    if (slon(x, y,'B')) result += "B";
     if (king(x, y)) result += "K";
-    if (horse(x, y)) result += "K";
+    if (horse(x, y)) result += "N";
     if (pishak(x, y)) result += "P";
-    if (slon(x, y) || tyra(x, y)) result += "Q";
-    if (tyra(x, y)) result += "R";
+    if (slon(x, y,'Q') || tyra(x, y,'Q')) result += "Q";
+    if (tyra(x, y,'R')) result += "R";
     return result;
 }
 
@@ -104,17 +104,18 @@ int main() {
     for (int i = 0; i < 8; i++) {
         cin >> board[i];
     }
-    cin;
     int q, x, y;
     cin >> q;
     for (int i = 0; i < q; i++) {
         cin >> x >> y;
         if(board[x-1][y-1]!='O')
-            cout<<board[x-1][y-1]<<endl;
-        string rez=find_result(x-1,y-1);
-        if(rez=="")
-            cout<<"O"<<endl;
-        else
-            cout<<rez<<endl;
+            cout<<"X"<<endl;
+        else {
+            string rez = find_result(x - 1, y - 1);
+            if (rez.empty())
+                cout << "O" << endl;
+            else
+                cout << rez << endl;
+        }
     }
 }
