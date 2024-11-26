839a8890d5fc02c89fc7755c1eb7d8c396bcd609
taras-ivanyk <taras.ivanyk.shi.2024@lpnu.ua>
2024-11-26 16:19:04 +0200

    Add some changes to vns_practice_work_1_task

--- ai_12/taras_ivanyk/saga_1/vns_practice_work_1_task_taras_ivanyk.cpp
+++ ai_12/taras_ivanyk/saga_1/vns_practice_work_1_task_taras_ivanyk.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
     const double x = 1.82;
     const double y = 18.5;
-    const double z = -3.4;
+    const double z = -1.7;
     double gamma, fi;
-    gamma = abs(pow(x,y/x) - pow(y/x, 1/3));
+    gamma = fabs(pow(x,y/x) - pow(y/x, 1/3));
     fi = (x - y) * (y - z/(y - x)) / (1 + pow( (y-x), 2));
     cout << "Gamma = " << gamma << ", Fi = " << fi;
 
