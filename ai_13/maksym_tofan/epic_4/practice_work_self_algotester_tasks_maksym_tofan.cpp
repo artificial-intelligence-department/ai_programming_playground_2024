a5eab244c111a19a8c4b87e311af5dd4969a8357
Maksym <maksym.tofan.shi.2024@lpnu.ua>
2024-11-28 21:39:29 +0200

    some chhanges

--- ai_13/maksym_tofan/epic_4/practice_work_self_algotester_tasks_maksym_tofan.cpp
+++ ai_13/maksym_tofan/epic_4/practice_work_self_algotester_tasks_maksym_tofan.cpp
@@ -4,13 +4,14 @@
 using namespace std;
 
 int main() {
-
     int count = 0;
     int size;
     cin >> size;
+    cin.ignore();  
     string line;
     vector<string> words;
-    for(int i = 0; i <= size; i++) {    
+
+    for (int i = 0; i < size; i++) {
         getline(cin, line);
         stringstream ss(line);
         string word;
@@ -18,21 +19,21 @@ int main() {
             words.push_back(word);
         }
     }
-    for(string elm : words) {  
-        if(elm.length() > 1){
-            for(int i = 0; i < elm.length(); i++) {
-                if(elm[i] == '#' && elm[i+1] != '#') {
+
+   
+    for (string& elm : words) {
+        if (elm.length() > 1) {
+            for (int i = 0; i < elm.length(); i++) {
+                if (elm[i] == '#' && elm[i+1] != '#') {
                     count += 1;
                 }
             }
-        } else if(elm[0] == '#') {  
+        } else if (elm[0] == '#') {
             count += 1;
         }
     }
 
     cout << count;
 
-
     return 0;
-
 }
