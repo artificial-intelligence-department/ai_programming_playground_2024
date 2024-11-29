5a1d90cb47beb1746418705454bde0906570685b
Bohdan Sylich <yfleyn@gmail.com>
2024-11-29 13:41:53 +0200

    Epic 5

--- ai_11/Bohdan_Sylich/Epic_5/algotester_lab_6_variant_3_bohdan_sylich.cpp
+++ ai_11/Bohdan_Sylich/Epic_5/algotester_lab_6_variant_3_bohdan_sylich.cpp
@@ -1,56 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <string>
-
+#include <algorithm>
 using namespace std;
-
-vector<int> findNumbers(const vector<string> &sudoku, int x, int y) {
-    vector<int> possibleNumbers;
-    int N = sudoku.size();
-    if (sudoku[x][y] != '0') {
-        possibleNumbers.push_back(sudoku[x][y] - '0');
-        return possibleNumbers;
-    }
-    set<int> usedNumbers;
-    for (int i = 0; i < N; i++) {
-        if (sudoku[x][i] != '0') usedNumbers.insert(sudoku[x][i] - '0');
-        if (sudoku[i][y] != '0') usedNumbers.insert(sudoku[i][y] - '0');
-    }
-    for (int num = 1; num <= N; num++) {
-        if (usedNumbers.find(num) == usedNumbers.end()) {
-            possibleNumbers.push_back(num);
-        }
-    }
-    return possibleNumbers;
-}
 
 int main() {
     int N;
     cin >> N;
-    vector<string> sudoku(N);
-    for (int i = 0; i < N; i++) {
-        cin >> sudoku[i];
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
     }
-    int Q;
-    cin >> Q;
-    vector<pair<int, int>> queries(Q);
-    
-    for (int i = 0; i < Q; i++) {
-        int x, y;
-        cin >> x >> y;
-        queries[i] = {x - 1, y - 1};
-    }
-
-    for (const auto &query : queries) {
-        int x = query.first;
-        int y = query.second;
-        vector<int> result = findNumbers(sudoku, x, y);
-        cout << result.size() << endl;
-        for (int num : result) {
-            cout << num << " ";
-        }
-        cout << endl;
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+        int modA = a % 3, modB = b % 3;
+        if (modA != modB) return modA < modB;
+        if (modA == 1) return a > b;
+        return a < b;
+    });
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    cout << arr.size() << endl;
+    for (int num : arr) {
+        cout << num << " ";
     }
     return 0;
 }
+
