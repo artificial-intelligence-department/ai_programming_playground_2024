db3d7cbb76e9670130797bbfd7a7d4aa3b843e06
Mykola Horishnyi <horishnyimykola1@gmail.com>
2024-11-30 07:41:56 +0200

    saga_1_practice_work_mykola_horishnyi

--- ai_12/mykola_horishnyi/epic_7/practice_work_self_algotester_6_mykola_horishnyi.cpp
+++ ai_12/mykola_horishnyi/epic_7/practice_work_self_algotester_6_mykola_horishnyi.cpp
@@ -2,30 +2,26 @@
 using namespace std;
 
 int main() {
-    int n; // кількість однокласників
+    int n;
     cin >> n;
 
-    int a[n]; // масив номерів будинків
+    int a[n];
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    int crossings = 0; // кількість переходів через дорогу
+    int crossings = 0; 
 
-    // Початковий бік дороги (парний чи непарний)
     bool isEvenSide = a[0] % 2 == 0;
 
-    // Перебираємо всі будинки і рахуємо переходи
     for (int i = 1; i < n; i++) {
         bool currentIsEven = a[i] % 2 == 0;
         if (currentIsEven != isEvenSide) {
-            crossings++; // Перехід через дорогу
+            crossings++; 
         }
-        // Оновлюємо бік дороги для наступного будинку
         isEvenSide = currentIsEven;
     }
 
-    // Виводимо кількість переходів
     cout << crossings << endl;
 
     return 0;
