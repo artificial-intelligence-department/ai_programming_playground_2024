e5d3fc150cd7eb6aad5283e5dec9ffb3c252a10a
Mykola Horishnyi <horishnyimykola1@gmail.com>
2024-12-02 19:41:31 +0200

    epic_5_mykola_horishnyi

--- ai_12/mykola_horishnyi/Epic_5_mykola_horishnyi/algotester_lab_4_variant_1_mykola_horishnyi.cpp
+++ ai_12/mykola_horishnyi/Epic_5_mykola_horishnyi/algotester_lab_4_variant_1_mykola_horishnyi.cpp
@@ -1,48 +1,180 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include <algorithm>
+
 using namespace std;
 
+void results(const int* result, int size)
+{
+    cout << size << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
+}
+
+void difference(const int* n, int N, const int* m, int M)
+{
+    int result[1000], resultSize = 0;
+
+    int i = 0, j = 0;
+    while (i < N && j < M)
+    {
+        if (n[i] < m[j])
+        {
+            result[resultSize++] = n[i++];
+        }
+        else if (n[i] > m[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < N)
+    {
+        result[resultSize++] = n[i++];
+    }
+
+    results(result, resultSize);
+}
+
+void section(const int* n, int N, const int* m, int M)
+{
+    int result[1000], resultSize = 0;
+
+    int i = 0, j = 0;
+    while (i < N && j < M)
+    {
+        if (n[i] < m[j])
+        {
+            i++;
+        }
+        else if (n[i] > m[j])
+        {
+            j++;
+        }
+        else
+        {
+            result[resultSize++] = n[i];
+            i++;
+            j++;
+        }
+    }
+
+    results(result, resultSize);
+}
+
+void unionNM(const int* n, int N, const int* m, int M)
+{
+    int result[2000], resultSize = 0;
+
+    int i = 0, j = 0;
+    while (i < N && j < M)
+    {
+        if (n[i] < m[j])
+        {
+            result[resultSize++] = n[i++];
+        }
+        else if (n[i] > m[j])
+        {
+            result[resultSize++] = m[j++];
+        }
+        else
+        {
+            result[resultSize++] = n[i];
+            i++;
+            j++;
+        }
+    }
+
+    while (i < N)
+    {
+        result[resultSize++] = n[i++];
+    }
+
+    while (j < M)
+    {
+        result[resultSize++] = m[j++];
+    }
+
+    results(result, resultSize);
+}
+
+void Difference(const int* n, int N, const int* m, int M)
+{
+    int result[2000], resultSize = 0;
+
+    int i = 0, j = 0;
+    while (i < N && j < M)
+    {
+        if (n[i] < m[j])
+        {
+            result[resultSize++] = n[i++];
+        }
+        else if (n[i] > m[j])
+        {
+            result[resultSize++] = m[j++];
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < N)
+    {
+        result[resultSize++] = n[i++];
+    }
+
+    while (j < M)
+    {
+        result[resultSize++] = m[j++];
+    }
+
+    results(result, resultSize);
+}
+
 int main() {
-    int N, K;
-    cin >> N >> K;
+    int N, M;
 
-    vector<int> a(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> a[i];
+    cin >> N;
+    int n[1000];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> n[i];
     }
 
-    // Видаляємо дублікати
-    set<int> unique_elements(a.begin(), a.end());
-    vector<int> unique_array(unique_elements.begin(), unique_elements.end());
-
-    // Сортуємо масив
-    sort(unique_array.begin(), unique_array.end());
-
-    // Обрізаємо значення K, якщо воно більше за розмір унікального масиву
-    K %= unique_array.size();
-
-    // Обертаємо масив на K
-    rotate(unique_array.begin(), unique_array.begin() + K, unique_array.end());
-
-  
-    cout << unique_array.size() << endl;
-    for (int num : unique_array) {
-        cout << num << " ";
+    cin >> M;
+    int m[1000];
+    for (int i = 0; i < M; i++)
+    {
+        cin >> m[i];
     }
-    cout << endl;
 
+    sort(n, n + N);
+    sort(m, m + M);
 
+    difference(n, N, m, M);
+    cout << "\n";
 
+    difference(m, M, n, N);
+    cout << "\n";
+
+    section(n, N, m, M);
+    cout << "\n";
+
+    unionNM(n, N, m, M);
+    cout << "\n";
+
+    Difference(n, N, m, M);
+    cout << "\n";
 
     return 0;
 }
-
-
-
-
-
-
-
 
