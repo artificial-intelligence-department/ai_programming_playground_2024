caa1e44854aad8ec535e362e946721e9ecb4310b
Denys Levchenko <denys.levchenko.shi.2024@lpnu.ua>
2024-12-03 10:36:56 +0200

    live_coding_event_1 #2 - Denys Levchenko

--- ai_11/denys_levchenko/saga_1/live_coding_event_1.cpp
+++ ai_11/denys_levchenko/saga_1/live_coding_event_1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <ctime>
 #include <algorithm>
+#include <limits>
 
 const int VARIANT = 15;
 using namespace std;
@@ -57,10 +58,41 @@ void function_2(double& x, double matrix[5][5]){
     }
 }
 
+int* function_3(int tower[5][5]){
+    static int result[5];
+
+    for (int col = 0; col < 5; col++) {
+        int min1 = numeric_limits<int>::max();
+        int min2 = numeric_limits<int>::max();
+
+        for (int row = 0; row < 5; row++) {
+            if (tower[row][col] < min1) {
+                min1 = tower[row][col];
+            }
+        }
+    min2 = *min_element(&tower[0][col], &tower[5][col]);
+    
+    if (min1 != min2){
+        cout << "search" << col + 1 << "not match" << endl;
+    } else {
+        cout << "search" << col + 1 << "match" << endl;
+    }
+
+    result[col] = min1;
+}
+    return result;
+}
 
 int main(){
     double x = function_1();
     double matrix[5][5];
     function_2(x, matrix);
+    int tower[5][5];
+    int* result = function_3(tower);
+    cout << "minimal elements: ";
+    for (int i = 0; i < 5; i++){
+        cout << result[i] << " ";
+    
+    }
     return 0;
 }
