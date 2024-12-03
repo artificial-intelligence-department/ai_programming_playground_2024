f35f333f879d1aaf87c3c815807bdd7dff36494b
Solomiia Andrusyshyn <solomiia.andrusyshyn.shi.2024@lpnu.ua>
2024-12-03 11:49:39 +0300

    2

--- ai_11/solomiia_andrusyshyn/Saga 1/live_coding_event_1.cpp
+++ ai_11/solomiia_andrusyshyn/Saga 1/live_coding_event_1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib> 
+#include <ctime>
 using namespace std;
 
 
@@ -68,10 +70,71 @@ double Function_1()
 
 }
 
+void Function_2 (double x, double[10][10]){
+    double arr[5][5] = {x};
+    for (int i = 0; i < 5; i++)
+    {
+       for (int j = 0; i < 5; i++) 
+       {
+        arr[i][j] = (arr[i][j] + 1)
+       }
+    }
+    void printMatrix(const double matrix[5][5])
+     {
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            std::cout << matrix[i][j] << "x";
+        }
+        std::cout << std::endl;
+    }
+}
+}
+void Function_3 (double x, double[10][10]){
+    srand(time(0)); // Ініціалізація генератора випадкових чисел
+
+    int arr[5][5];
+    int rowSums[7], colSums[4];
+
+    Fill_Array(arr);
+
+    cout << "Масив:" << endl;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << arr[i][j] << "";
+        }
+        cout << endl;
+    }
+
+    Sum_by_rows(arr, rowSums);
+    Sum_by_columns(arr, colSums);
+
+    cout << "\nСуми по рядках:" << endl;
+    for (int i = 0; i < 7; i++) {
+        cout << "Рядок " << i + 1 << ": " << rowSums[i] << endl;
+    }
+
+    cout << "\nСуми по стовпцях:" << endl;
+    for (int j = 0; j < 4; j++) {
+        cout << "Стовпець " << j + 1 << ": " << colSums[j] << endl;
+    }
+
+
+
+
+
+
+}
+
+
+
+
 int main()
 {
+    srand(time(0));
     double x = Function_1();
     cout << x;
+
+    double 
 
 return 0;
 }
