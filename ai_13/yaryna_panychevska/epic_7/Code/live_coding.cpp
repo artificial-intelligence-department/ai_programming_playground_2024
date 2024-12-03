7eb307c94457ec464ab0e147d6edbf7898059a87
Yaryna Panychevska <pa.yaryna@gmail.com>
2024-12-03 10:35:56 +0200

    Live_Coding_event_1 - Yaryna Panychevska

--- ai_13/yaryna_panychevska/epic_7/Code/live_coding.cpp
+++ ai_13/yaryna_panychevska/epic_7/Code/live_coding.cpp
@@ -17,12 +17,12 @@ double function_1() {
     cin >> f;
 
     double nums[6];
-     nums[0] = a;
-     nums[1] = b;
-     nums[2] = c;
-     nums[3] = d;
-     nums[4] = e;
-     nums[5] = f;
+    nums[0] = a;
+    nums[1] = b;
+    nums[2] = c;
+    nums[3] = d;
+    nums[4] = e;
+    nums[5] = f;
 
     int n = sizeof(nums) / sizeof(nums[0]);
     sort(nums, nums + 6);
@@ -37,10 +37,10 @@ double function_1() {
     if (sumMax > sumMin * 2) {
         result = sumMax * sumMin;
     }
-    else if(sumMax > sumMin * 3) {
+    else if (sumMax > sumMin * 3) {
         result = sumMax / sumMin;
     }
-    else if(sumMax > sumMin * 4) {
+    else if (sumMax > sumMin * 4) {
         result = sumMax + sumMin;
     }
 
@@ -51,33 +51,53 @@ double function_1() {
 
 void function_2(double &x, double matrix[5][5]) {
     //заповнення матриці за значення x
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
             matrix[i][j] = x;
         }
     }
-//Оновлення матриці за формулою
-    for(int i = 0; i < 5; i++) {
+    //Оновлення матриці за формулою
+
+    for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            matrix[i][j] = (matrix[i][j] + VARIANT + j - i)* rand(1, 100);
+            matrix[i][j] = (matrix[i][j] + VARIANT + j - i) * rand(1, 100);
         }
     }
-
     //оновленя матриця за формулою(Виведення)
-    for(int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
             cout << matrix[i][j] << "\t";
         }
     }
 }
 
+void function_3(double matrix[5][5], double arr[5]) {
+    //Знаходження найменшого елемента матриці з стовпця
+    double minCols;
+    for(int j = 0; j < 5; j++) {
+        double minCols = matrix[0][j];
+    }
 
+    for (int j = 0; j < 5; j++) {
+        for (int i = 1; i < 5; i++) {
+            if (matrix[i][j] < minCols) {
+                minCols = matrix[i][j];
+            }
+        }
+       double arr[j] = minCols;
+    }
+
+    cout << "The smallest values in each column: " << arr[j] << " " << endl;
+
+}
 int main() {
-double result_x = function_1();
+    double result_x = function_1();
 
-double x = result_x;
-double matrix[5][5];
-function_2(x, matrix);
+    double x = result_x;
+    double matrix[5][5];
+    function_2(x, matrix);
 
-return 0;
+    double arr[5];
+    function_3(matrix, arr);
+    return 0;
 }
