ac7a18bb2068ccc5f2368e4815c18ce767cbfc21
Yurа Klymchuk <fuyrnature2007@gmail.com>
2024-12-03 10:33:09 +0200

    Live coding - second commit

--- ai_11/yurii_klymchuk/Live_coding_event_1.cpp
+++ ai_11/yurii_klymchuk/Live_coding_event_1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bits/stdc++.h> 
 #include <stdlib.h>
+#include <string>
+#include <fstream>
 using namespace std;
 
 float function_1(){
@@ -32,12 +34,11 @@ float function_1(){
     }
 
     float fin = x/11;
-    cout << fin;
+    cout << fin << endl;
 
     return fin;
 }
 
-//нове_значення=(поточне_значення+номер_варіанту+індекс_стовбця−індекс_стрічки)∗rand(1,100
 float** function_2(float x){
     float** matrix= new float*[5];
 
@@ -62,15 +63,104 @@ float** function_2(float x){
     return matrix;
 }
 
-// float function_3(float** array){
+float* function_3(float** matrix){
+    float* array_sort1 = new float[5];
 
-// }
+    for(int i = 0; i < 5; i++){
+        float minimal = 10000000;
+        for(int j = 0; j < 5; j++){
+            if(matrix[j][i] < minimal){
+                minimal = matrix[j][i];
+                array_sort1[i] = matrix[j][i];
+                
+            }
+        } 
+    }
+
+    float* array_sort2 = new float[5];
+    for (int i = 0; i < 5; i++){
+        array_sort2[i] = min(matrix[0][i], matrix[4][i]);
+    }
+
+    bool identical = true;
+    for (int i = 0; i < 5; i++){
+        if (array_sort1[i] != array_sort2[i]){
+            identical = false;
+            break;
+        }
+    }
+
+    return array_sort1;
+}
+
+
+struct Student{
+    string name;
+    string surname;
+    float money;
+};
+
+// struct Node {
+//     Student data;          
+//     Node* prev;        
+//     Node* next;         
+
+    
+//     Node(Student value.name) : data.name(value.name), prev(nullptr), next(nullptr) {}
+// };
+
+
+// class DoublyLinkedList {
+// private:
+//     Node* head; 
+//     Node* tail; 
+
+// public:
+//     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+//     void append(int value) {
+//         Node* newNode = new Node(value);
+//         if (!head) {
+//             head = tail = newNode;
+//         } else {
+//             tail->next = newNode;
+//             newNode->prev = tail;
+//             tail = newNode;
+//         }
+//     }
+
+//     void prepend(int value) {
+//         Node* newNode = new Node(value);
+//         if (!head) {
+//             head = tail = newNode;
+//         } else {
+//             newNode->next = head;
+//             head->prev = newNode;
+//             head = newNode;
+//         }
+//     }
+
+//     ~DoublyLinkedList() {
+//         Node* current = head;
+//         while (current) {
+//             Node* temp = current;
+//             current = current->next;
+//             delete temp;
+//         }
+//     }
+// };
+void function_5(){
+
+}
 int main() {
     float x = function_1();
 
     float** tower;
     tower = function_2(x);
 
+    float* min_array;
+    min_array = function_3(tower);
 
+    
     return 0;
 }
