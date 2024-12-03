b16d04a0ef60c76cb824aff9c72744e9f7a826d6
Natalya <nataliia.kozak.shi.2024@lpnu.ua>
2024-12-03 10:30:06 +0200

    did more tasks

--- ai_13/nataliia_kozak/Saga_1/live_coding/live_coding_event_1.cpp
+++ ai_13/nataliia_kozak/Saga_1/live_coding/live_coding_event_1.cpp
@@ -9,7 +9,7 @@ float function_1()
     double f;
     cin >> a >> b >> c >> d >> e >> f;
     double array[6] = {a, b, (double)c, (double)d, (double)e, f};
-    sort(array, array + 5);
+    sort(array, array + 6);
     double max_sum = 0, min_sum = 0;
     for (int i = 0; i < 3; i++)
     {
@@ -82,33 +82,79 @@ ARRAY_2D function_2(float &x)
     }
     return arr;
 }
-float* function_3(ARRAY_2D matrix){
-    float* a1 = new float[5];
-    float* a2 = new float[5];
+float *function_3(ARRAY_2D matrix)
+{
+    float *a1 = new float[5];
+    float *a2 = new float[5];
     float min;
-    for(int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         min = matrix.array[0][i];
-        for(int j = 0; j < 5 ; j++){
-            if(matrix.array[j][i] < min){
+        for (int j = 0; j < 5; j++)
+        {
+            if (matrix.array[j][i] < min)
+            {
                 min = matrix.array[j][i];
             }
         }
         a1[i] = min;
     }
-    for(int i = 0; i < 5; i++){
-        sort(matrix.array[i], matrix.array[i] + 4);
-        a2[i] = matrix.array[i][0];
+    float column[5];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            column[j] = matrix.array[j][i];
+        }
+        sort(column, column + 5);
+        a2[i] = column[0];
     }
-    for(int i = 0 ; i < 5; i++){
-        if(a1[i] != a2[i]){
-            cout << "Results not equal";
+    for (int i = 0; i < 5; i++)
+    {
+        if (a1[i] != a2[i])
+        {
+            cout << endl
+                 << "Results not equal";
+            // break;
         }
     }
+    return a1;
+}
+struct Student{
+    string name, surname;
+    float money;
+    Student* next;
+    Student* previous;
+};
+Student* function_4(float* a){
+    Student* array = new Student[7];
+    for(int i = 0; i < 7; i++){
+        array[i].name = "Nata";
+        array[i].surname = "Kozak";
+    }
+    sort(a, a+5);
+    array[0].money = a[4];
+    float sum = 0;
+    int index = 0;
+    for(int i = 1; i < 6; i++){
+        sum += a[index++];
+        array[i].money = sum;
+    }
+    array[6].money = a[0];
+    for(int i = 0; i < 7; i++){
+        cout << endl;
+        cout << array[i].name << " "<< array[i].surname << " "<< array[i].money;
+    }
+    return array;
 }
 int main()
 {
     float x = function_1();
     ARRAY_2D tower;
     tower = function_2(x);
-    function_3(tower);
+    float *array;
+    array = function_3(tower);
+    Student* stud_array;
+    stud_array = function_4(array);
+    function_5
 }
