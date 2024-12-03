2182122d7d35bacb3cf0f1356f3583098f8a6d29
Nazik <nazikkryvycko@gmail.com>
2024-12-03 10:30:57 +0200

    Live coding event #1 - Nazar Kryvychko

--- ai_12/nazar_kryvychko/saga_1/liveCoding/live_coding_event_1.cpp
+++ ai_12/nazar_kryvychko/saga_1/liveCoding/live_coding_event_1.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
 const double taskN = 40; // Варіант
+
+
+struct Node
+{
+    public:
+        Student val;
+        Node* next;
+        Node(Student val, Node* next)
+        {
+            this->val = val;
+            this->next = next;
+        }
+    Node() { }
+};
 
 double function_1()
 {
@@ -92,13 +107,54 @@ double** function_2(double& val)
 //Find min (each row // HERE // )
 double minEl(double* array)
 {
-
+    double minElement = array[0];
+    for (size_t i = 1; i < 5; i++)
+    {
+        if(array[i] < minElement)
+            minElement = array[i];
+    }
+    return minElement;
 }
 
 
 
+void printArray(double* arr)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    
+}
 
-void function_3(double** tower)
+
+//Student Struct
+struct Student
+{
+    std::string name , surname;
+    double balance;
+    Student(std::string name, std::string surname, double balance)
+    {
+        this->name = name;
+        this->surname = surname;
+        this->balance = balance;
+    }
+};
+
+void function_4(double* resArray)
+{
+    Student s1("Nazar", "Kryvychko", 20.00);
+    Student s2("Taras", "Ivannyk", 37.79);
+    Student s3("Stepan", "Kobula", 11.24);
+    Student s4("Georgiy", "Vyxo", 17.88);
+    Student s5("Bogdan", "Sosnulo", 18.22);
+    Student s6("Stepan", "Shuika", 100.23);
+    Student s7("Sergiy", "Tokaruk", 78.11);
+    
+    Node* headLL = new Node(s1, new Node(s2,new Node(s3,new Node(s4,new Node(s5,new Node(s6, new Node(s7,nullptr)))))));
+}
+
+double* function_3(double** tower)
 {
     double* oneDimenArray = new double[5];
     double resultMinEl1[5] , resultMinEl2[5];
@@ -121,11 +177,31 @@ void function_3(double** tower)
         {
             columnArray[j] = tower[j][i];
         }
+        resultMinEl2[i] = minEl(columnArray);
         
     }
+
+    std::cout << "Array 1: " << std::endl;
+    printArray(resultMinEl1);
+    std::cout << std::endl;
+    std::cout << "Array 2: " << std::endl;
+    printArray(resultMinEl2);
+
+    for (int i = 0; i < 5; i++)
+    {
+        if(resultMinEl1[i] != resultMinEl2[i])
+        {
+            std::cerr << "I am so bad at coding! Result are not equal!!1!1!" << std::endl;
+            return resultMinEl1;
+        }
+    }
+    std::cout << "Everything is Good!" << std::endl;
+    
     
 
     delete oneDimenArray;
+
+    return resultMinEl1;
     
 }
 
@@ -139,7 +215,13 @@ int main(void)
 
     //Test for function_2
     double** tower = function_2(resXF1);
+
+    //Task function_3
+    double* resultF3 = function_3(tower);
     
+
+
+
 
     for (size_t i = 0; i < 5; i++)
     {
