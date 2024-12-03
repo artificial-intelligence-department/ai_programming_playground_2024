cde94341b04d7f1b1079aaf360496aaa79e2ed53
Oleh Stanko <Themorphio@gmail.com>
2024-12-03 10:36:10 +0200

    live coding event#1 - Oleh stanko

--- ai_11/oleh_stanko/saga_1/live_coding_event_1.cpp
+++ ai_11/oleh_stanko/saga_1/live_coding_event_1.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
 
-void first_exercise()
+const int var = 24;
+
+double function_1()
 {
     float a,b;
     cin >> a >> b;
@@ -29,10 +32,55 @@ void first_exercise()
             break;
         }
     }
-    cout << arr[0];
+    cout << arr[0] << arr[1] << arr[2] << endl;
+    cout << arr[3] << arr[4] << arr[5] << endl;
+    double smax = arr[0] + arr[1] + arr[2];
+    double smin = arr[3] + arr[4] + arr[5];
+    
+    double x=0;
+    if (smax > smin*4)
+    {
+        x = smax + smin;
+    }
+    else if (smax > smin*3)
+    {
+        x = smax / smin;
+    }
+    else if (smax > smin*2)
+    {
+        x=smax*smin;
+    }  
+    x /= var;
+    cout << x;
+    return x;
 } 
+
+double function_2(double* x)
+{
+    double matrix[5][5];
+    for (int i; i<5; i++)
+    {
+        for (int j; j<5; j++)
+        {
+            matrix[i][j]=*x;
+        }
+    }
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 500);
+
+    for (int i; i<5; i++)
+        {
+        for (int j; j<5; j++)
+        {
+            matrix[i][j]=((matrix[i][j]+var+j-i)*rand()%100+1);
+
+        }
+    }  
+}
 
 int main()
 {
-    first_exercise();
+    double x = function_1();
+    function_2(&x);
 }
