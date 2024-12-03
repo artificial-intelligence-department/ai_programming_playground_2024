50bc36edcd1b60b887ca5d7eb916deed08cee14d
Danylo Kolbasiuk <danya.kolbasiuk@gmail.com>
2024-12-03 10:19:51 +0200

    Live_Coding_Event#1 - Danylo Kolbasiuk(2)

--- ai_13/danylo_kolbasiuk/saga_1/live_coding_event_1.cpp
+++ ai_13/danylo_kolbasiuk/saga_1/live_coding_event_1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <algorithm>
-const int var=67;
-struct student
+const int var=67; // 4. в коді використана як мінімум одна цілочисельна константа
+struct student // 19. в коді використано свою структуру даних
 {
     std::string name;
     std::string surname;
     float money;
-}
+};
 float function_1()
 {
-    float a,b;
-    int c,d,e;
-    double f;
+    float a,b; // 2. в коді використана як мінімум одна дійсна змінна
+    int c,d,e; // 1. в коді використана як мінімум одна цілочисельна змінна
+    double f; // 3.  в коді використана як мінімум одна дійсний з подвійною точністю змінна
     std::cin >> a >> b;
-    std::cin >> c >> d >> e;
+    std::cin >> c >> d >> e; // 18. в коді використано оператори виведення та введення даних
     std::cin >> f;
     float maxs=0;
     float mins=0;
@@ -22,7 +22,7 @@ float function_1()
     maxs=arr[3]+arr[4]+arr[5];
     mins=arr[0]+arr[1]+arr[2];
     float result=0;
-    if (maxs>(mins*2))
+    if (maxs>(mins*2)) // в коді використані умовні оператори та розгалуження
     {
         result=maxs*mins;
     }
@@ -39,9 +39,10 @@ float function_1()
     return result;
 }
 
-float** function_2(float &x)   
+float** function_2(float &x)  
 {
-    float **array=new float* [5];
+    float **array=new float* [5]; // 20. в коді використано вказівники 
+    // 7. в коді використаний двовимірний масив 
     for(int i=0; i<5; i++)
     {
         array[i]=new float [5];
@@ -74,9 +75,9 @@ float** function_2(float &x)
 float* function_3(float** tower)
 {
     float* array1=new float [5];
-    float* array2=new float [5];
+    float* array2=new float [5]; // 6. в коді використаний одновимірний масив 
     
-    for(int i=0; i<5; i++)
+    for(int i=0; i<5; i++) // 10. в коді використаний for цикл 
     {
         float min1=tower[i][0];
         for(int j=0; j<5; j++)
