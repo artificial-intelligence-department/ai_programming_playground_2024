930287a2108ee4048ce25e03660a8f29e4220db0
Mykhailo Skichko <delfineon@gmail.com>
2024-12-03 10:48:37 +0200

    latest changes

--- ai_13/mykhailo_skichko/saga_1/tasks/livecoding/live_coding_event_1_mykhailo_skichko.cpp
+++ ai_13/mykhailo_skichko/saga_1/tasks/livecoding/live_coding_event_1_mykhailo_skichko.cpp
@@ -4,8 +4,75 @@
 #include <cstdlib>
 #include <fstream>
 #include <cmath>
+#include <climits>
 
 const int myVariant = 78;
+
+double partition(double array[], int lb, int ub)
+{
+    double pivot = array[lb];
+    int start = lb;
+    int end = ub;
+    while (start < end)
+    {
+        while (array[start] <= pivot)
+        {
+            start++;
+        }
+        while (array[end] > pivot)
+        {
+            end--;
+        }
+        if (start < end)
+        {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+        }
+    }
+    int temp = array[end];
+    array[end] = array[lb];
+    array[lb] = temp;
+    return end;
+}
+
+void quick_sort(double array[], double lb, double ub)
+{
+    while (lb < ub)
+    {
+        int loc = partition(array, lb, ub);
+        quick_sort(array, loc + 1, ub);
+        quick_sort(array, lb, loc - 1);
+    }
+}
+
+int getMinValue(int array[], int size)
+{
+    int minValue = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] < minValue)
+        {
+            minValue = array[i];
+        }
+    }
+    return minValue;
+}
+
+double getMinValue(double *array, int size)
+{
+    double minValue = INT_MIN;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] < minValue)
+        {
+            minValue = array[i];
+        }
+    }
+    return minValue;
+}
 
 double function_1()
 {
@@ -52,12 +119,12 @@ double function_1()
     return x;
 }
 
-double function_2(const double &x)
+double **function_2(const double &x)
 {
-    int **matrix = new int *[5];
+    double **matrix = new double *[5];
     for (int count = 0; count < 5; ++count)
     {
-        matrix[count] = new int[5];
+        matrix[count] = new double[5];
     }
 
     for (int i = 0; i < 5; ++i)
@@ -85,13 +152,76 @@ double function_2(const double &x)
         std::cout << std::endl;
     }
 
-    return 0;
+    return matrix;
+}
+
+double *function_3(double **tower)
+{
+    double *resultantArray = new double[5];
+
+    double *minValues_v1 = new double[5];
+    double *minValues_v2 = new double[5];
+
+    for (int i = 0; i < 5; ++i)
+    {
+        minValues_v1[i] = getMinValue(tower[i], 5);
+    }
+
+    for (int i = 0; i < 5; ++i)
+    {
+        quick_sort(tower[i], 0, 5);
+        minValues_v2[i] = tower[i][0];
+    }
+
+    int ind = 0;
+    while (minValues_v1[ind] == minValues_v2[ind])
+    {
+        ++ind;
+        if (ind == 5)
+        {
+            break;
+        }
+    }
+    if (ind == 5)
+    {
+        std::cout << "results match" << std::endl;
+    }
+    else
+    {
+        std::cout << "results don't match" << std::endl;
+    }
+
+    for (int i = 0; i < 5; ++i)
+    {
+        resultantArray[i] = minValues_v1[i];
+    }
+
+    return resultantArray;
+}
+
+double function_4()
+{
+
+    return 0.0;
+}
+
+double function_5()
+{
+
+    return 0.0;
+}
+
+double function_6()
+{
+
+    return 0.0;
 }
 
 int main()
 {
     double x = function_1();
-    function_2(x);
+    double **tower = function_2(x);
+    double *minValues = function_3(tower);
 
     return 0;
 }
