b458d3d6c47b975cdc9827f58cba091e8b3e510a
marriiikkkk <markiian.lytvyn.shi.2024@lpnu.ua>
2024-12-03 10:37:31 +0200

    Live_Coding_event#1_#2 - Markiian Lytvyn

--- ai_13/markiian_lytvyn/saga_7/live_coding_event_1.cpp
+++ ai_13/markiian_lytvyn/saga_7/live_coding_event_1.cpp
@@ -9,14 +9,14 @@ float function_1(){
     float a, b; // Дійсна змінна
     int c, d, e; // Цілочисельна змінна
     double f; // Дійсна з подвійною точністю змінна
-    cin >> a >> b >> c >> d >> e >> f;
+    cin >> a >> b >> c >> d >> e >> f; //Оператор введення даних
     double arr1[6] = {a, b, c, d, e, f}; // Одновимірний масив
     sort(arr1[0], arr1[5]);
     float min_sum = arr1[0] + arr1[1] + arr1[2];
     float max_sum= arr1[3] + arr1[4] + arr1[5];
     float y;
     if (max_sum > (min_sum * 2)) { // Умовний оператор
-        y = max_sum * min_sum;
+        y = max_sum * min_sum; // Використано математичні операції та функції
     }
     if (max_sum > (min_sum * 3)){
         y = max_sum * min_sum;
@@ -24,8 +24,8 @@ float function_1(){
     if (max_sum > (min_sum * 4)){
         y = max_sum + min_sum;
     }
-    float x = y/option; 
-    cout << x << endl;
+    float x = y/option;  // Використано параметри та аргументи функції
+    cout << x << endl; //Оператор виведення даних
     return x;
 }
 
@@ -53,11 +53,32 @@ float **function_2(float& x){ //вказівник
 float *function_3(float **tower){
     float *arr3 = new float[5];
     float *arr4 = new float[5];
-    float small;
     for (int i = 0; i < 5; i++){
+        float small = tower[i][0];
         for (int j = 0; j < 5; j++){
-            
+            if (small > tower[i][j]){
+                small = tower[i][j];
+            }
         }
+    float minim;
+    for (int j = 0; j < 5; j++){
+        minim = min(tower[0][j], tower[0][j] + 5);
+        arr4[j]= minim;
+    }
+    for (int i = 0; i < 5; i++){
+        if (arr3[i] = arr4[i]){
+            bool equal = true;
+        } else {
+            bool equal = false;
+            break; // break
+        }
+    }
+    if (bool equal = true){
+        cout << "Mins are equal";
+    }
+    else {
+        cout << "Not equal";
+    }
     }
 }
 
