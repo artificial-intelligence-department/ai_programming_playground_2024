7ca13306c549bfbab1cfc0f85a9d767be527f899
bubelnyk_yura <urabubelnik@gmail.com>
2024-12-03 10:38:08 +0200

    live-coding_2

--- ai_11/yurii_bubelnyk/epic_7/live_coding/live_coding_event_1.cpp
+++ ai_11/yurii_bubelnyk/epic_7/live_coding/live_coding_event_1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+#include <limits>
 using namespace std;
+
+const int VAR = 3;
 double function_1();
 void Sort(vector<double>& arr);
 vector<vector<double>> function_2(double &x);
 void ShowArray(vector<vector<double>> &arr, double x);
+void findMinAl(vector<vector<double>> &arr, vector<double> minAr);
+void findMin(vector<vector<double>> &arr, vector<double> minAr);
+vector<vector<double>> function_3(vector<vector<double>> &arr);
+
 
 int main()
 {
     srand(time(NULL));
     double x = function_1(); 
-    vector<vector<double>> matrix = function_2(x); 
-    ShowArray(matrix, x);
+    vector<vector<double>> tower = function_2(x); 
+    ShowArray(tower, x);
 
     return 0;
 }
@@ -62,12 +69,32 @@ vector<vector<double>> function_2(double &x)
     {
         for (int j = 0; j < 5; j++)
         {
-            arr[i][j] = (x + 3 + j - i) * (rand() % 100 + 1);
+            arr[i][j] = (x + VAR + j - i) * (rand() % 100 + 1);
         }    
     }  
 
     return arr;
      
+}
+vector<vector<double>> function_3(vector<vector<double>> &arr)
+{
+    vector<double> minAr1(5, numeric_limits<double>::max());
+    vector<double> minAr2(5, numeric_limits<double>::max());
+    findMin(arr, minAr1);
+    findMinAl(arr, minAr2);
+
+    bool equal = true;
+    for (int i = 0; i < 5; i++)
+    {
+        if (minAr1[i] != minAr2[i])
+        {
+            equal = false;
+            break;
+        }
+        
+    }
+    
+
 }
 void ShowArray(vector<vector<double>> &arr, double x)
 {
@@ -92,6 +119,31 @@ void Sort(vector<double>& arr)
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
+        }
+    }
+}
+
+void findMin(vector<vector<double>> &arr, vector<double> minAr)
+{   
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            if (arr[j][i] < minAr[j])
+            {
+                minAr[j] = arr[j][i];
+            }    
+        }
+    }
+}
+
+void findMinAl(vector<vector<double>> &arr, vector<double> minAr)
+{   
+    for (int i = 0; i < 5; i++){
+        minAr[i] = arr[0][i];
+        for (int j = 1; j < 5; j++){
+            if (arr[j][i] < minAr[j])
+            {
+                minAr[j] = arr[j][i];
+            }    
         }
     }
 }
