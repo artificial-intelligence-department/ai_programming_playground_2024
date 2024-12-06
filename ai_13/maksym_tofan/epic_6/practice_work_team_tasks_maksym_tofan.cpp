9dbd918638e1d3ade265a14fe42bf0e9dd3839a2
Maksym <maksym.tofan.shi.2024@lpnu.ua>
2024-12-06 18:49:09 +0200

    Epic 6 - Maksym Tofan

---
+++ ai_13/maksym_tofan/epic_6/practice_work_team_tasks_maksym_tofan.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+// Двонаправлений список
+struct DCLNode {
+    T data;
+    DCLNode* next;
+    DCLNode* prev;
+    DCLNode(T value) : data(value), next(nullptr), prev(nullptr) {}
+
+    DCLNode<T>& operator<<(T value) {
+        DCLNode<T>* temp = new DCLNode<T>(value);
+        next = temp;
+        temp->prev = this;
+        return *next;
+    }
+};
+
+// Бінарне дерево пошуку
+template <typename T>
+struct BSTNode {
+    T data;
+    BSTNode* left;
+    BSTNode* right;
+    BSTNode(T value) : data(value), left(nullptr), right(nullptr) {}
+
+    void insert(T value) {
+        if (value < data) {
+            if (!left) left = new BSTNode<T>(value);
+            else left->insert(value);
+        } else if (value > data) {
+            if (!right) right = new BSTNode<T>(value);
+            else right->insert(value);
+        }
+    }
+
+    void print() {
+        if (left) left->print();
+        cout << data << " ";
+        if (right) right->print();
+    }
+};
+
+// Завдання 1: Реверсування списку
+template <typename T>
+DCLNode<T>* reverse(DCLNode<T>* head) {
+    DCLNode<T>* prev = nullptr;
+    DCLNode<T>* curr = head;
+    while (curr) {
+        DCLNode<T>* next = curr->next;
+        curr->next = prev;
+        if (prev) prev->prev = curr;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+template <typename T>
+void output(DCLNode<T>* node) {
+    while (node) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+// Завдання 2: Порівняння двох списків
+template <typename T>
+bool compare(DCLNode<T>* list1, DCLNode<T>* list2) {
+    while (list1 && list2) {
+        if (list1->data != list2->data) return false;
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+    return list1 == nullptr && list2 == nullptr;
+}
+
+// Завдання 3: Додавання двох чисел, представлених списками
+template <typename T>
+DCLNode<T>* add(DCLNode<T>* node1, DCLNode<T>* node2) {
+    int num1 = 0, num2 = 0, multiplier = 1;
+
+    while (node1 || node2) {
+        if (node1) {
+            num1 += node1->data * multiplier;
+            node1 = node1->next;
+        }
+        if (node2) {
+            num2 += node2->data * multiplier;
+            node2 = node2->next;
+        }
+        multiplier *= 10;
+    }
+
+    int sum = num1 + num2;
+    DCLNode<T>* result = new DCLNode<T>(sum % 10);
+    DCLNode<T>* current = result;
+    sum /= 10;
+
+    while (sum) {
+        current->next = new DCLNode<T>(sum % 10);
+        current = current->next;
+        sum /= 10;
+    }
+
+    return result;
+}
+
+// Завдання 4: Створення дзеркального відображення дерева
+template <typename T>
+BSTNode<T>* mirror(BSTNode<T>* root) {
+    if (!root) return nullptr;
+    BSTNode<T>* temp = root->left;
+    root->left = mirror(root->right);
+    root->right = mirror(temp);
+    return root;
+}
+
+// Завдання 5: Обчислення суми елементів дерева
+int tree_sum(BSTNode<int>* root) {
+    if (!root) return 0;
+    return root->data + tree_sum(root->left) + tree_sum(root->right);
+}
+
+int main() {
+    // Завдання 1
+    DCLNode<int> list1(3);
+    list1 << 2 << 4 << 6 << 7;
+    output(&list1);
+    DCLNode<int>* reversed = reverse(&list1);
+    output(reversed);
+
+    // Завдання 2
+    DCLNode<int> list2(1);
+    list2 << 2 << 3 << 4 << 5 << 6;
+    DCLNode<int> list3(1);
+    list3 << 2 << 3 << 4 << 5;
+    cout << (compare(&list2, &list3) ? "\nlist2 and list3 are similar\n" : "\nlists are different\n");
+
+    // Завдання 3
+    DCLNode<int> list4(1);
+    list4 << 3 << 7 << 9;
+    DCLNode<int> list5(1);
+    list5 << 2 << 5;
+    DCLNode<int>* list6 = add(&list4, &list5);
+    output(list6);
+
+    // Завдання 4
+    BSTNode<int> tree1(5);
+    tree1.insert(11);
+    tree1.insert(64);
+    tree1.insert(57);
+    tree1.insert(68);
+    tree1.print();
+    cout << "\nMirror of tree1: ";
+    mirror(&tree1)->print();
+
+    // Завдання 5
+    cout << "Tree sum: " << tree_sum(&tree1) << endl;
+
+    return 0;
+}
---
+++ ai_13/maksym_tofan/epic_6/vns_lab_10_task_maksym_tofan.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Структура вузла списку
+struct Node {
+    string data;
+    Node* prev;
+    Node* next;
+};
+
+// Функція для створення нового вузла
+Node* createNode(const string& value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->prev = nullptr;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+// Функція для додавання елемента після вузла зі вказаним символом
+void addAfter(Node*& head, char symbol, const string& value) {
+    Node* current = head;
+    while (current) {
+        if (!current->data.empty() && current->data[0] == symbol) {
+            Node* newNode = createNode(value);
+            newNode->next = current->next;
+            newNode->prev = current;
+            if (current->next) {
+                current->next->prev = newNode;
+            }
+            current->next = newNode;
+            return;
+        }
+        current = current->next;
+    }
+    cout << "Елемент з символом '" << symbol << "' не знайдено.\n";
+}
+
+// Функція для знищення K перших елементів
+void deleteFirstK(Node*& head, int k) {
+    for (int i = 0; i < k && head; ++i) {
+        Node* temp = head;
+        head = head->next;
+        if (head) {
+            head->prev = nullptr;
+        }
+        delete temp;
+    }
+}
+
+// Функція для друку списку
+void printList(Node* head) {
+    Node* current = head;
+    while (current) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+// Функція для запису списку у файл
+void saveToFile(Node* head, const string& filename) {
+    ofstream outFile(filename);
+    if (!outFile) {
+        cerr << "Не вдалося відкрити файл для запису.\n";
+        return;
+    }
+    Node* current = head;
+    while (current) {
+        outFile << current->data << endl;
+        current = current->next;
+    }
+    outFile.close();
+}
+
+// Функція для відновлення списку з файлу
+void loadFromFile(Node*& head, const string& filename) {
+    ifstream inFile(filename);
+    if (!inFile) {
+        cerr << "Не вдалося відкрити файл для читання.\n";
+        return;
+    }
+    string line;
+    Node* tail = nullptr;
+    while (getline(inFile, line)) {
+        Node* newNode = createNode(line);
+        if (!head) {
+            head = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+        }
+        tail = newNode;
+    }
+    inFile.close();
+}
+
+// Функція для очищення всього списку
+void deleteList(Node*& head) {
+    while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+int main() {
+    Node* head = nullptr;
+
+    // Створення списку
+    head = createNode("apple");
+    head->next = createNode("banana");
+    head->next->prev = head;
+    head->next->next = createNode("cherry");
+    head->next->next->prev = head->next;
+
+    cout << "Початковий список: ";
+    printList(head);
+
+    // Додавання елемента
+    addAfter(head, 'b', "blueberry");
+    cout << "Після додавання 'blueberry' після 'banana': ";
+    printList(head);
+
+    // Знищення K перших елементів
+    deleteFirstK(head, 2);
+    cout << "Після видалення 2 перших елементів: ";
+    printList(head);
+
+    // Запис у файл
+    saveToFile(head, "list.txt");
+    cout << "Список записано у файл 'list.txt'.\n";
+
+    // Очищення списку
+    deleteList(head);
+    cout << "Список очищено.\n";
+
+    // Відновлення списку з файлу
+    loadFromFile(head, "list.txt");
+    cout << "Список відновлено з файлу: ";
+    printList(head);
+
+    // Очищення пам'яті
+    deleteList(head);
+
+    return 0;
+}
