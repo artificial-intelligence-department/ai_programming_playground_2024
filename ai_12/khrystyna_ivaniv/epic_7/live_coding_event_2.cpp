10014bb85b2bab3aa4fa1f83f50f952412011233
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2024-12-12 10:42:04 +0200

    LCE2 - 12 Ivaniv Khrystyna

--- ai_12/khrystyna_ivaniv/epic_7/live_coding_event_2.cpp
+++ ai_12/khrystyna_ivaniv/epic_7/live_coding_event_2.cpp
@@ -1,29 +1,3 @@
-/**
- * Ви отримали своє перше завдання на новій роботі програмістом у
- * міжнародній логістичній компанії, що здійснює доставку між відділеннями.
- * Раніше офіси компанії працювали незалежно і лише в межах однієї країни,
- * але компанія запустила послугу "Комбінована доставка" і тепер розрахунок вартості
- * відбувається згідно тарифікації, що зберігається у файлах.
- * Все б нічого, але офіси використовують 3 різні системи вимірювання відстані:
- * - імперська (британська миля) == 1 609.344 метра; // mi
- * - миля США (US survey mile) == 1 609.347 метра; // mi_US
- * - метрична система виміру == 1 метр. // m
- *
- * Розробити програму для розрахунку вартості доставки,
- * яка буде використовувати ідентифікатор користувача та накопичувати відстані.
- * Компанія встановила наступні бонуси:
- * Якщо відстань відправлення є паліндромом, то користувач отримує +200 км бонусів.
- * Кожні накопичені 100км це 1$ знижки. Після використання накопичена відстань зменшується.
- * При обрахунку вартості запропонувати користувачу скористатись конвертацією накопичених бонусів.
-
-
- * Для формування ідентифікатора користувача використати наступний розрахунок:
-    1. Відсортувати буĸви вашого імені за зростанням.
-    2. Створити одновимірний масив на основі цілочисельного(int) значення
-    символів (ASCII Value) помноженого на 0.4.
-
-    Результат записати.
- */
 #include <iostream>
 #include <iomanip>
 #include <cstring>
@@ -34,13 +8,14 @@
 
 using namespace std;
 
-bool is_palindrome(int number)
+const int var = 37;            // одна цілочисельна константа
+bool is_palindrome(int number) // параметри та аргументи функції
 {
 
    int origin_number = number;
    int reverse = 0;
    int carry;
-   while (number != 0)
+   while (number != 0) // while цикл
    {
       carry = number % 10;
       reverse = reverse * 10 + carry;
@@ -54,7 +29,7 @@ double simulate_money_precision(double value)
    return round(value * 100.0) / 100.0;
 }
 
-double convert_to_meters(double distance, char *type)
+double convert_to_meters(double distance, char *type) //вказівники
 {
    if (strcmp(type, "meter") == 0)
    {
@@ -72,9 +47,10 @@ double convert_to_meters(double distance, char *type)
 
    else
    {
-      cout << "Ви вказали некоректний тип даних";
+      cout << "Ви вказали некоректний тип даних" << endl;
       return 1;
    }
+
    return 0.0;
 }
 
@@ -82,7 +58,8 @@ void sort_main(char name[], int len)
 {
    for (int i = 0; i < len - 1; i++)
    {
-      for (int j = 0; j < len - 1 - i; j++)
+      for (int j = 0; j < len - 1 - i; j++) // for цикл
+
       {
          if (name[j] > name[j + 1])
          {
@@ -100,20 +77,44 @@ void sort_main(char name[], int len)
 
 int main()
 {
+
    string user_name;
-   int length = user_name.length();
-   char name[length + 1];
+   int length = user_name.length(); // цілочисельна змінна
+   char name[length + 1];           // одновимірний масив
    for (int i = 0; i < length; i++)
    {
       name[i] = user_name[i];
    }
 
-   double distance_value;
+   ofstream out_file("user.txt"); //функції роботи з файлами, для того, щоб записати у файл
+   out_file << user_name;
+   out_file.close();
+
+   ifstream in_file("user.txt"); //функції роботи з файлами, для того, щоб зчитати з файлу
+   while (in_file)
+   {
+      string line;
+      while (getline(in_file, line))
+      {
+         cout << line << endl;
+      }
+   }
+   in_file.close();
+
+   float distance_value; // дійсна змінна
    string measurement_system;
+      int length1 = measurement_system.length(); // цілочисельна змінна
+   char measurement_system_char[length1 + 1];           // одновимірний масив
+   for (int i = 0; i < length1; i++)
+   {
+      measurement_system_char[i] = measurement_system[i];
+   }
+measurement_system_char[length1] = '\0';
+
    bool convert_bonus;
 
-   cout << "Введіть ваше ім'я: ";
-   cin >> user_name;
+   cout << "Введіть ваше ім'я: "; // оператори виведення та введення даних
+   cin >> user_name; //оператори виведення та введення даних
 
    cout << "Введіть відстань: ";
    cin >> distance_value;
@@ -123,10 +124,34 @@ int main()
    cout << "Бажаєте конвертувати бонуси у відстань (y/n)? ";
    cin >> convert_bonus;
 
-   // cout << "Відстань поточної доставки: " << deliveryDistanceInMeters << "m" << endl;
-   // cout << "Ваша знижка: " << discount << "$" << endl;
-   // cout << "Накопичена відстань: " << deliveryDistanceInKiloMeters << "km" << endl;
-   // cout << "Вартість відправки: " << totalPrice << "$" << endl;
+   is_palindrome(int(distance_value)); // параметри та аргументи функції	
+
+       int bonuses = 0;
+   // умовні оператори та розгалуження
+   if (is_palindrome)
+   {
+      bonuses += 2000;
+   }
+
+   bonuses /= 1000;
+   int carry;
+   int discount = 0;
+   if (convert_bonus)
+   {
+      carry = bonuses % 100; // математичні операції та математичні функції
+      discount = bonuses % 100;
+   }
+
+   double total_price = convert_to_meters(distance_value, measurement_system_char) * 0.5; // дійсна змінна з подвійною точністю
+   // ціна за кожен метр
+
+double deliveryDistanceInKiloMeters = double(distance_value) / 1000;
+
+   total_price = total_price - discount;
+   cout << "Відстань поточної доставки: " << convert_to_meters << "m" << endl;
+   cout << "Ваша знижка: " << discount << "$" << endl;
+   cout << "Накопичена відстань: " << deliveryDistanceInKiloMeters << "km" << endl;
+   cout << "Вартість відправки: " << total_price << "$" << endl;
 
    return 0;
 }
