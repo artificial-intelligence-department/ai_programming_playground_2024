7eaef8093229b7a446d0d8ecfbc99891f33205b4
What-I-pressed <139636603+What-I-pressed@users.noreply.github.com>
2024-12-12 10:17:22 +0200

    Saga 2 - Anton Tsymbaliuk

--- ai_13/anton_tsymbaliuk/saga_2/live_coding_event_2.cpp
+++ ai_13/anton_tsymbaliuk/saga_2/live_coding_event_2.cpp
@@ -17,6 +17,12 @@ bool isPalindrome(double distance){
     return true;
 }
 
+double calculateDiscount(double distance){
+    double discount = 0;
+    if(distance - 100 * 1000 > 0) discount += 1 + calculateDiscount(distance - 100 * 1000);
+    return discount;
+}
+
 
 int main(){
     std::fstream fMile("mile.txt", std::ofstream::out | std::ofstream::in);
@@ -24,64 +30,97 @@ int main(){
     std::fstream fUS_Mile("us_mile.txt", std::ofstream::out | std::ofstream::in );
     fUS_Mile << 1.2;
     std::fstream fMetr("mext.txt", std::ofstream::out | std::ofstream::in);
+    std::fstream fCollectedDistance("fCollectedDistance.txt",  std::ofstream::out | std::ofstream::in);
+    
+    
     fMetr << 0.0005;
     fMile.seekp(0);
     fUS_Mile.seekp(0);
     fMetr.seekp(0);
     
-    std::string username;
-    std::cout << "Enter username : ";
-    std::cin >> username;
-    std::cout << "Enter distance : ";
-    double distance;
-    std::cin >> distance;
-    std::cout << "Enter unit of measurment(meter/mile/mile_us) : ";
-    std::string measurmentSystem;
-    std::cin >> measurmentSystem;
-    std::string IDstring = username;
-    bool sorted = false;
-    do{
-        sorted = true;
-        for(int i = 0; i < IDstring.size() - 1; i++) if(IDstring[i] > IDstring[i+1]){
-            sorted = false;
-            std::swap(IDstring[i], IDstring[i + 1]);
-        }
+    while(true){
+        std::string username;
+        std::cout << "Enter username : ";
+        std::cin >> username;
+        std::cout << "Enter distance : ";
+        double distance;
+        std::cin >> distance;
+        std::cout << "Enter unit of measurment(meter/mile/mile_us) : ";
+        std::string measurmentSystem;
+        std::cin >> measurmentSystem;
+        std::string IDstring = username;
+        bool sorted = false;
+        do{
+            sorted = true;
+            for(int i = 0; i < IDstring.size() - 1; i++) if(IDstring[i] > IDstring[i+1]){
+                sorted = false;
+                std::swap(IDstring[i], IDstring[i + 1]);
+            }
 
-    }while(!sorted);
+        }while(!sorted);
 
 
-    int IDarr[sizeof(IDstring)];
-    for(int i = 0; i < IDstring.size(); i++) IDarr[i] = IDstring[i] * 0.4;
+        int IDarr[sizeof(IDstring)];
+        for(int i = 0; i < IDstring.size(); i++) IDarr[i] = IDstring[i] * 0.4;
 
-    
-    std::string convertStr;
-    std::cout << "Do you want to convert your bonuses(y/n) : ";
-    std::cin >> convertStr;
-    bool convert = convertStr == "y" ? 1 : 0;
-    double distanceInMeters;
-    double price;
-    double pricePerMeasure;
-    if(measurmentSystem == "meter"){
-        fMetr >> pricePerMeasure;
-        distanceInMeters = distance;
-    }
-    else if(measurmentSystem == "mile"){
-        fMile >> pricePerMeasure;
-        distanceInMeters = distance * 1609.344;
-    }
-    else{
-        fUS_Mile >> pricePerMeasure;
-        distanceInMeters = distance * 1609.347;
-    }
-    price = distance * pricePerMeasure;
-    std::fstream collectedDistance("collectedDistance.txt",  std::ofstream::out | std::ofstream::in);
-    collectedDistance.seekp(0);
-    if(convert){
         
+        std::string convertStr;
+        std::cout << "Do you want to convert your bonuses(y/n) : ";
+        std::cin >> convertStr;
+        bool convert = convertStr == "y" ? 1 : 0;
+        double distanceInMeters;
+        double price;
+        double pricePerMeasure;
+        if(measurmentSystem == "meter"){
+            fMetr >> pricePerMeasure;
+            distanceInMeters = distance;
+        }
+        else if(measurmentSystem == "mile"){
+            fMile >> pricePerMeasure;
+            distanceInMeters = distance * 1609.344;
+        }
+        else{
+            fUS_Mile >> pricePerMeasure;
+            distanceInMeters = distance * 1609.347;
+        }
+        price = distance * pricePerMeasure;
+        fCollectedDistance.seekp(0);
+        double collectedDistance = 0;
+
+        std::cout << "Distance in meters : " << distanceInMeters << std::endl;
+        std::string strArr = "";
+        if(convert){
+            std::string ID;
+            fCollectedDistance >> ID;
+                if(ID == IDstring){
+                    fCollectedDistance >> ID;
+                    
+                    for(int i = 0; i < IDstring.size(); i++){
+                        strArr += IDarr[i];
+                    }
+                    if(ID == strArr){
+                        fCollectedDistance >> ID;
+                        collectedDistance = std::stod(ID);
+                    }
+                }
+        }
+        double discount = calculateDiscount(collectedDistance);
+        bool ispalindrome = isPalindrome(distance);
+        if (ispalindrome) discount += 2;
+        if(discount > price) discount = price;
+        std::cout << "Discount : " << discount + (isPalindrome(distance) ? 200 : 0) << std::endl; 
+        std::cout << "Collected distance " << collectedDistance / 1000 << " km"<< std::endl;
+        std::cout << "Price " << price << " $\n";
+        double collected = collectedDistance - discount * 100000 + distanceInMeters + (ispalindrome ? 200000 : 0);
+        fCollectedDistance.seekp(0);
+        fCollectedDistance << IDstring << "\n";
+        fCollectedDistance << strArr << "\n";
+        fCollectedDistance << collected;
     }
+    
 
 
-
+    fCollectedDistance.close();
     fMile.close();
     fUS_Mile.close();
     fMetr.close();
