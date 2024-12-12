11f9ce119f4140e39771dabc33b7f84abb36fc5f
Nazik <nazikkryvycko@gmail.com>
2024-12-12 10:37:46 +0200

    LE2 - stage 2

--- ai_12/nazar_kryvychko/saga_2/liveCoding_2/life_coding_event2.cpp
+++ ai_12/nazar_kryvychko/saga_2/liveCoding_2/life_coding_event2.cpp
@@ -33,9 +33,79 @@
 
 
 // For switch case simplicity
+
+
+
 enum measurement {
     mile , mile_us , meter , none
 };
+
+measurement parseMeasurement(std::string measurementStr)
+{
+    if(measurementStr == "mile")
+    {
+        return measurement::mile;
+    }
+    else if (measurementStr == "mile_us")
+    {
+        return measurement::mile_us;
+    }
+    else if(measurementStr == "meter")
+    {
+        return measurement::meter;
+    }
+    else 
+    {
+        return measurement::none;
+    }
+}
+
+
+
+
+struct user
+{
+    std::string name;
+    int collected;
+    measurement meas;
+};
+
+std::string createUsersFormatString(user* users, const size_t& size)
+{
+    std::string str;
+    for (size_t i = 0; i < size; i++)
+    {
+        str.append(users[i].name + " " + std::to_string(users[i].collected) + " " + std::to_string(users[i].meas));
+    }
+    
+}
+
+user parseUser(std::string lineToParse) // alex 1912194 meter
+{
+    std::istringstream iss(lineToParse);
+    std::string s;
+    std::string els[3];
+    unsigned int index = 0;
+    while ( getline( iss, s, ' ' ) ) {
+        els[index++] = s;
+    }
+    user u;
+    u.name = els[0];
+    u.collected = std::stoi(els[1]);
+    u.meas = parseMeasurement(els[2]);
+    return u;
+}
+
+bool isPalindrome(int distance)
+{
+    std::string distanceStr = std::to_string(distance);
+    for (size_t i = 0; i < distanceStr.size()/2; i++)
+    {
+        if(distanceStr[i] != distanceStr[distanceStr.size()-1-i]) return false;
+    }
+    return true;
+    
+}
 
 void bubbleSort(std::string arr, const size_t& size)
 {
@@ -66,25 +136,7 @@ double* identifier(const char* array, const size_t& size)
     return identifierPtr;
 }
 
-measurement parseMeasurement(std::string measurementStr)
-{
-    if(measurementStr == "mile")
-    {
-        return measurement::mile;
-    }
-    else if (measurementStr == "mile_us")
-    {
-        return measurement::mile_us;
-    }
-    else if(measurementStr == "meter")
-    {
-        return measurement::meter;
-    }
-    else 
-    {
-        return measurement::none;
-    }
-}
+
 
 int main(void)
 {
@@ -99,7 +151,6 @@ int main(void)
     M_POINT:
     std::cout << "Enter your measurement system (meter/mile/mile_us): ";
     getline(std::cin, measurementSystem);
-    std::cout << measurementSystem;
     measurement currentMeasure = parseMeasurement(measurementSystem);
     // Edge case
     if(currentMeasure == measurement::none)
@@ -118,7 +169,7 @@ int main(void)
     const double mi = 1609.344 , mi_US = 1609.347 , m = 1; 
     double pricePerMi, pricePerMiUs , pricePerMeter;
 
-    //Gettig coefs in respect to meter
+    //Gettig price of each unit
     std::string coefBuffer;
     meterFile >> coefBuffer;
     pricePerMeter = std::stod(coefBuffer);
@@ -130,14 +181,79 @@ int main(void)
 
     std::cout << "Enter order distance: ";
     std::cin >> distance;
+    std::cin.ignore();
     std::cout << "Do you want to reedem current bonuses (y/n) : ";
     getline(std::cin, tmpString);
     wantGetBonuses = tmpString == "y" ? true : false;
     unsigned int bonuses = 0;
+    double finalPrice;
+    switch (currentMeasure)
+    {
+    case measurement::meter:
+        finalPrice = pricePerMeter * distance;
+        break;
+    case measurement::mile:
+        finalPrice = pricePerMi * distance;
+        break;
+    case measurement::mile_us:
+        finalPrice = pricePerMiUs * distance;
+        break;
+    
+    default:
+        break;
+    }
+
+    //Read existing records in FIle to array users
+    std::ifstream collectedInfo("C:\\IT\\Politex\\Epic_1\\src\\ai_programming_playground_2024\\ai_12\\nazar_kryvychko\\saga_2\\liveCoding_2\\collected_distances.txt");
+    std::ofstream collectedInfoOut("C:\\IT\\Politex\\Epic_1\\src\\ai_programming_playground_2024\\ai_12\\nazar_kryvychko\\saga_2\\liveCoding_2\\collected_distances.txt");
+    std::string fileLine;
+    user* users;
+    int count = 0;
+    while(std::getline(collectedInfo,fileLine))
+    {
+        count++;
+    }
+
+    users = new user[count];
+    unsigned int index = 0;
+
+    //TO the start
+    collectedInfo.clear();
+    collectedInfo.seekg(0);
+    while(std::getline(collectedInfo,fileLine))
+    {
+        users[index++] = parseUser(fileLine);
+    }
+
+    //Palindrome bonus
+    if(isPalindrome(distance))
+    {
+        bonuses += 2;
+    }
+
+
     if(wantGetBonuses)
     {
-        // ... TODO redeem current bonuses
+        for (size_t i = 0; i < count; i++)
+        {
+            if(users[i].name == name)
+            {
+
+                int collectedBonuses =  (int) (users[i].collected / 100);
+                bonuses += collectedBonuses;
+                // Redem bonuses
+                users[i].collected -= (collectedBonuses * 100);
+                goto AfterPOINT;
+            }
+        }
+        std::cerr << "User not found!\n";
+        
     }
+    AfterPOINT:
+    //Rewrite current file:
+
+
+
 
     // Generating identifier
     std::string nameCopy = name;
@@ -145,9 +261,21 @@ int main(void)
     double* userIdentifier = identifier(nameCopy.c_str(), name.size());
 
 
+
+    std::cout << "Your discount: " << bonuses << "$" << std::endl;
+    std::cout << "Order Price: " << finalPrice << "$" << std::endl;
+    std::cout << "Final Price: " << finalPrice - bonuses << "$" << std::endl;
+
     
 
+    
+    meterFile.close();
+    mileFile.close();
+    mileUsFile.close();
+    collectedInfo.close();
+
     delete[] userIdentifier;
+    delete[] users;
 
     return 0;
 }
