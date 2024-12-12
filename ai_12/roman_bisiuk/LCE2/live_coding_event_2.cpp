ecbdd4e035e1672658e5fbebcee2e73fd0d58b97
Roman Bisiuk <roman.bisiuk.shi.2024@lpnu.ua>
2024-12-12 10:17:38 +0200

    Second half

--- ai_12/roman_bisiuk/LCE2/live_coding_event_2.cpp
+++ ai_12/roman_bisiuk/LCE2/live_coding_event_2.cpp
@@ -11,7 +11,7 @@ const int meter = 1; //цілочисельна константа
 const double mile = 1 609.344; //дійсний з подвійною точністю змінна
 const double mile_us = 1609.347; 
 
-bool isPalyndrome(double number){
+bool isPalyndrome(double number){ // параметри та аргументи функції	
     string str = to_string(number);
     int n = str.size();
     for(int i = 0; i < n / 2 i++){ // for цикл
@@ -22,7 +22,7 @@ bool isPalyndrome(double number){
     return true;
 }
 
-void bubbleSort(char* str, int n){
+void bubbleSort(char* str, int n){ //використано вказівники
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < n - i - 1; j++){
             if(str[j] > str[j +1])
@@ -33,8 +33,23 @@ void bubbleSort(char* str, int n){
     }
 }
 
-double readFromFile(const string& fileName){
-    ifstream file(fileName);
+void gemerateUserId(contst string& name){ // параметри та аргументи функції
+    int n = name.size();
+    char sortedName[n]; //одновимірний масив 
+    for(int i = 0; i < n; i++){
+        sortedName[i] = name[i];
+    }
+
+    bubbleSort(sortedName, n);
+    for(int i = 1; i <= n; i++){
+    int value = static_cast<int>(sortedName[i]) * 0.4;
+    cout << value " ";
+    }
+    cout << endl;
+}
+
+double readFromFile(const string& fileName){ 
+    ifstream file(fileName); //функції роботи з файлами, для того, щоб зчитати з файлу
     if(!file){
         cerr << "Помилка: не вдалося відкрити файл" << endl;
         return 0;
@@ -49,7 +64,7 @@ void createFiles(){
     ofstream file;
     file.open("price_per_meter.txt");
     if(file){
-        file << 0.0005;
+        file << 0.0005; // функції роботи з файлами, для того, щоб записати у файл
         file.close();
     }else{
         cerr << "Помилка: не вдалося відкрити файл" << endl;
@@ -77,13 +92,14 @@ int main(){
     string userName;
     double distanceValue;
     string measurementSystem;
+    int discount; // цілочисельна змінна
 
     double pricePerMeter = readFromFile("price_per_meter.txt");
     double pricePerMile = readFromFile("price_per_mile.txt");
     double pricePerMileUs = readFromFile("price_per_mile_us.txt");
 
     cout << "Введіть ваше ім'я: ";
-    cin >> userName;
+    cin >> userName; // оператори виведення та введення даних
 
     cout << "Введіть відстань: ";
     cin >> distanceValue;
@@ -91,13 +107,25 @@ int main(){
     cout << "Введіть одиницю виміру (meter/mile/mile_us): ";
     cin >> measurementSystem;
 
+    float price;
     if(measurementSystem == 'meter'){
-        float price = pricePerMeter * distanceValue;
+        price = pricePerMeter * distanceValue;
     }else if(measurementSystem == 'mile'){
-        float price = pricePerMile * distanceValue;
+        price = pricePerMile * distanceValue;
     }else{
-        float price = pricePerMileUs * distanceValue;
+        price = pricePerMileUs * distanceValue; // дійсна змінна
     }
+    int deliveryDistanceInMeters;
+    int deliveryDistanceInKiloMeters;
+    if(measurementSystem = 'mile'){
+        deliveryDistanceInMeters = mile_us * meter;
+        deliveryDistanceInKiloMeters = deliveryDistanceInMeters / 1000;
+    }
+    if(measurementSystem = 'mile_us'){
+        deliveryDistanceInMeters = mile_us * meter;
+        deliveryDistanceInKiloMeters = deliveryDistanceInMeters / 1000;
+    }
+    float totalPrice = price - discount;
 
     cout << "Відстань поточної доставки: " << deliveryDistanceInMeters << "m" << endl;
     cout << "Ваша знижка: " << discount << "$" << endl;
