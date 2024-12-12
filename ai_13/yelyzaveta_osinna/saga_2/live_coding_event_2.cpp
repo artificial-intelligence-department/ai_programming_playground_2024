e114c687b3f6a1d0e13e0081b653dc71bebd1e75
Yelyzaveta Osinna <yelyzaveta.osinna.shi.2024@lpnu.ua>
2024-12-12 10:41:34 +0200

    LCE2 - shi-13 Osinna Yelyzaveta changes

--- ai_13/yelyzaveta_osinna/saga_2/live_coding_event_2.cpp
+++ ai_13/yelyzaveta_osinna/saga_2/live_coding_event_2.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 const double MI_TO_M = 1609.344;
 const double MI_US_TO_M = 1609.344;
+const double KM_BONUS = 200000;
+
+string reverseString(string str, int start, int end) { 
+    while(start < end) {
+        swap(str[start], str[end]);
+        start++;
+        end--;
+    }
+    return str;
+}
 
 bool isPalindrom(string distanceValue) {
     
@@ -13,18 +24,6 @@ bool isPalindrom(string distanceValue) {
     return distanceValue == rev;
 }
 
-string reverseString(string& str, int start, int end) { 
-    if (start >= end) { 
-        return; 
-    } 
-     
-    char temp = str[start]; 
-    str[start] = str[end]; 
-    str[end] = temp; 
-     
-    reverseString(str, start + 1, end - 1); 
-    return str;
-}
 
 double simulateMoneyPrecision(double value) {
     return round(value * 100.0) / 100.0;
@@ -70,13 +69,35 @@ int main() {
     else if (measurementSystem == "mile_us") {
         disInM = distanceValue*MI_TO_M;
     }
+
+    if(isPalindrom(to_string(static_cast<int>(distanceValue)))) {
+        distanceValue += KM_BONUS;
+    }
     
     string convertBonus;
     cout << "Бажаєте конвертувати бонуси у відстань (y/n)? ";
     cin >> convertBonus;
 
-    
+    for (int i = 0; i < userName.length(); i++) {
+        cout << idArray[i];
+    }
+    cout << "   " << distanceValue;
 
+    double deliveryDistanceInMeters, discount, deliveryDistanceInKiloMeters, totalPrice;
+
+    ofstream MyFile("price_per_meter.txt");
+    MyFile << "0.0005";
+    MyFile.close();
+
+    ofstream MyFile("price_per_mile.txt");
+    MyFile << "0.08";
+    MyFile.close();
+
+    ofstream MyFile("price_per_mile_us.txt");
+    MyFile << "1.2";
+    MyFile.close();
+
+    
 
     cout << "Відстань поточної доставки: " << deliveryDistanceInMeters << "m" << endl;
     cout << "Ваша знижка: " << discount << "$" << endl;
